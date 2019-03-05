c96e3126c7f9207d4886ae6cce26d434082b296a
Johannes Kliemann <kliemann@componolit.com>
2019-03-05 13:54:20 +0100

    unify c++ implementation and types

--- platform/genode/block.h
+++ platform/genode/block.h
@@ -6,50 +6,15 @@
 
 namespace Block
 {
-    class Client
+    enum Kind {NONE, READ, WRITE, SYNC};
+    enum Status {RAW, OK, ERROR, ACK};
+    struct Request
     {
-        private:
-            Genode::uint64_t _device;
-            Genode::uint64_t _block_count;
-            Genode::uint64_t _block_size;
-
-        public:
-
-            enum Kind {NONE, READ, WRITE, SYNC};
-            enum Status {
-                RAW,
-                OK,
-                ERROR,
-                ACK
-            };
-
-            struct Request
-            {
-                Kind kind;
-                Genode::uint8_t uid[16];
-                Genode::uint64_t start;
-                Genode::uint64_t length;
-                Status status;
-            };
-
-            Client();
-            void initialize(const char *device = nullptr);
-            void finalize();
-            void submit_read(Request req);
-            void submit_sync(Request req);
-            void submit_write(
-                    Request req,
-                    Genode::uint8_t *data,
-                    Genode::uint64_t length);
-            Request next();
-            void read(
-                    Request &req,
-                    Genode::uint8_t *data,
-                    Genode::uint64_t length);
-            void acknowledge(Request req);
-            bool writable();
-            Genode::uint64_t block_count();
-            Genode::uint64_t block_size();
+        Kind kind;
+        Genode::uint8_t uid[16];
+        Genode::uint64_t start;
+        Genode::uint64_t length;
+        Status status;
     };
 }
 
