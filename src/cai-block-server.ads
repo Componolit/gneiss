db2dca1373b50e4b30864f2645c1fc98e9ac8055
Johannes Kliemann <kliemann@componolit.com>
2019-03-06 12:50:12 +0100

    fix function and procedure signatures

--- src/cai-block-server.ads
+++ src/cai-block-server.ads
@@ -4,19 +4,19 @@ generic
    with procedure Acknowledge (D : in out Component.Block_Server_Device; R : Request; C : Context);
 package Cai.Block.Server is
 
-   procedure Initialize (D : in out Component.Block_Server_Device; L : String; C : Context);
+   procedure Initialize (D : out Component.Block_Server_Device; L : String; C : Context);
 
    procedure Finalize (D : in out Component.Block_Server_Device);
 
-   function Block_Count (D : in out Component.Block_Server_Device) return Count;
+   function Block_Count (D : Component.Block_Server_Device) return Count;
 
-   function Block_Size (D : in out Component.Block_Server_Device) return Size;
+   function Block_Size (D : Component.Block_Server_Device) return Size;
 
-   function Writable (D : in out Component.Block_Server_Device) return Boolean;
+   function Writable (D : Component.Block_Server_Device) return Boolean;
 
-   function Maximal_Transfer_Size (D : in out Component.Block_Server_Device) return Unsigned_Long;
+   function Maximal_Transfer_Size (D : Component.Block_Server_Device) return Unsigned_Long;
 
-   procedure Read (D : in out Component.Block_Server_Device; B : out  Buffer; R : in out Request);
+   procedure Read (D : in out Component.Block_Server_Device; B : out Buffer; R : in out Request);
 
    procedure Sync (D : in out Component.Block_Server_Device; R : in out Request);
 
