cdc1c842e338d31b5c2dbdf1ea4dcd0e6830eff5
Johannes Kliemann <kliemann@componolit.com>
2019-04-04 16:19:57 +0200

    calling a procedure with an initialized session results in an initialized session

--- src/cai-block-client.ads
+++ src/cai-block-client.ads
@@ -22,9 +22,10 @@ is
       end case;
    end record;
 
-   function Create return Client_Session;
-
    function Initialized (C : Client_Session) return Boolean;
+
+   function Create return Client_Session with
+      Post => not Initialized (Create'Result);
 
    function Get_Instance (C : Client_Session) return Client_Instance with
       Pre => Initialized (C);
@@ -34,7 +35,8 @@ is
                          Buffer_Size :        Byte_Length := 0);
 
    procedure Finalize (C : in out Client_Session) with
-      Pre => Initialized (C);
+      Pre  => Initialized (C),
+      Post => not Initialized (C);
 
    function Ready (C : Client_Session;
                    R : Request) return Boolean with
@@ -46,44 +48,49 @@ is
 
    procedure Enqueue_Read (C : in out Client_Session;
                            R :        Request) with
-      Pre => Initialized (C)
-             and then R.Kind = Read
-             and then R.Status = Raw
-             and then Ready (C, R)
-             and then Supported (C, R);
+      Pre  => Initialized (C)
+              and then R.Kind = Read
+              and then R.Status = Raw
+              and then Ready (C, R)
+              and then Supported (C, R),
+      Post => Initialized (C);
 
    procedure Enqueue_Write (C : in out Client_Session;
                             R :        Request;
                             B :        Buffer) with
-      Pre => Initialized (C)
-             and then R.Kind = Write
-             and then R.Status = Raw
-             and then B'Length = R.Length * Block_Size (C)
-             and then Ready (C, R)
-             and then Supported (C, R);
+      Pre  => Initialized (C)
+              and then R.Kind = Write
+              and then R.Status = Raw
+              and then B'Length = R.Length * Block_Size (C)
+              and then Ready (C, R)
+              and then Supported (C, R),
+      Post => Initialized (C);
 
    procedure Enqueue_Sync (C : in out Client_Session;
                            R :        Request) with
-      Pre => Initialized (C)
-             and then R.Kind = Sync
-             and then R.Status = Raw
-             and then Ready (C, R)
-             and then Supported (C, R);
+      Pre  => Initialized (C)
+              and then R.Kind = Sync
+              and then R.Status = Raw
+              and then Ready (C, R)
+              and then Supported (C, R),
+      Post => Initialized (C);
 
    procedure Enqueue_Trim (C : in out Client_Session;
                            R :        Request) with
-      Pre => Initialized (C)
-             and then R.Kind = Trim
-             and then R.Status = Raw
-             and then Ready (C, R)
-             and then Supported (C, R);
+      Pre  => Initialized (C)
+              and then R.Kind = Trim
+              and then R.Status = Raw
+              and then Ready (C, R)
+              and then Supported (C, R),
+      Post => Initialized (C);
 
    procedure Submit (C : in out Client_Session) with
-      Pre => Initialized (C);
+      Pre  => Initialized (C),
+      Post => Initialized (C);
 
    function Next (C : Client_Session) return Request with
       Volatile_Function,
-      Pre => Initialized (C),
+      Pre  => Initialized (C),
       Post => (if Next'Result.Kind /= None
                then Next'Result.Status = Ok or Next'Result.Status = Error
                else True);
@@ -91,16 +98,18 @@ is
    procedure Read (C : in out Client_Session;
                    R :        Request;
                    B : out    Buffer) with
-      Pre => Initialized (C)
-             and then R.Kind = Read
-             and then R.Status = Ok
-             and then B'Length >= R.Length * Block_Size (C);
+      Pre  => Initialized (C)
+              and then R.Kind = Read
+              and then R.Status = Ok
+              and then B'Length >= R.Length * Block_Size (C),
+      Post => Initialized (C);
 
    procedure Release (C : in out Client_Session;
                       R : in out Request) with
-      Pre => Initialized (C)
-             and then R.Kind /= None
-             and then (R.Status = Ok or R.Status = Error);
+      Pre  => Initialized (C)
+              and then R.Kind /= None
+              and then (R.Status = Ok or R.Status = Error),
+      Post => Initialized (C);
 
    function Writable (C : Client_Session) return Boolean with
       Pre => Initialized (C);
