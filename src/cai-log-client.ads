393f8aa29b3f77d05b303f68fc8070102ab09264
Johannes Kliemann <kliemann@componolit.com>
2019-04-05 11:05:30 +0200

    ease prove for minimal message length

--- src/cai-log-client.ads
+++ src/cai-log-client.ads
@@ -15,31 +15,31 @@ is
       Pre  => Initialized (C),
       Post => not Initialized (C);
 
+   Minimal_Message_Length : constant Positive := 78 with Ghost;
+
    function Maximal_Message_Length (C : Client_Session) return Integer with
       Pre  => Initialized (C),
-      Post => Maximal_Message_Length'Result > 78;
-
-   function Valid_Message (C   : Client_Session;
-                           Msg : String) return Boolean is
-      (Msg'Length < Maximal_Message_Length (C)) with
-      Pre => Initialized (C);
+      Post => Maximal_Message_Length'Result > Minimal_Message_Length;
 
    procedure Info (C       : in out Client_Session;
                    Msg     :        String;
                    Newline :        Boolean := True) with
-      Pre  => Initialized (C) and then Valid_Message (C, Msg),
+      Pre  => Initialized (C) and then (Msg'Length <= Minimal_Message_Length
+                                        or else Msg'Length <= Maximal_Message_Length (C)),
       Post => Initialized (C);
 
    procedure Warning (C       : in out Client_Session;
                       Msg     :        String;
                       Newline :        Boolean := True) with
-      Pre  => Initialized (C) and then Valid_Message (C, Msg),
+      Pre  => Initialized (C) and then (Msg'Length <= Minimal_Message_Length
+                                        or else Msg'Length <= Maximal_Message_Length (C)),
       Post => Initialized (C);
 
    procedure Error (C       : in out Client_Session;
                     Msg     :        String;
                     Newline :        Boolean := True) with
-      Pre  => Initialized (C) and then Valid_Message (C, Msg),
+      Pre  => Initialized (C) and then (Msg'Length <= Minimal_Message_Length
+                                        or else Msg'Length <= Maximal_Message_Length (C)),
       Post => Initialized (C);
 
    procedure Flush (C : in out Client_Session) with
