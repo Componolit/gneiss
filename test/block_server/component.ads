3774a370d8702095229516b91752b730b0259eff
Johannes Kliemann <kliemann@componolit.com>
2019-04-08 15:58:28 +0200

    add block server and block proxy tests

---
+++ test/block_server/component.ads
@@ -0,0 +1,37 @@
+
+with Cai.Component;
+with Cai.Block;
+with Cai.Block.Server;
+with Cai.Block.Dispatcher;
+
+package Component is
+
+   procedure Construct;
+
+   package Server_Component is new Cai.Component (Construct);
+
+   type Byte is mod 2 ** 8;
+   type Buffer is array (Long_Integer range <>) of Byte;
+
+   package Block is new Cai.Block (Byte, Long_Integer, Buffer);
+
+   procedure Event;
+   function Block_Count (S : Block.Server_Instance) return Block.Count;
+   function Block_Size (S : Block.Server_Instance) return Block.Size;
+   function Writable (S : Block.Server_Instance) return Boolean;
+   function Maximal_Transfer_Size (S : Block.Server_Instance) return Block.Byte_Length;
+   procedure Initialize (S : Block.Server_Instance; L : String);
+   procedure Finalize (S : Block.Server_Instance);
+
+   procedure Request;
+
+   package Block_Server is new Block.Server (Event,
+                                             Block_Count,
+                                             Block_Size,
+                                             Writable,
+                                             Maximal_Transfer_Size,
+                                             Initialize,
+                                             Finalize);
+   package Block_Dispatcher is new Block.Dispatcher (Block_Server, Request);
+
+end Component;
