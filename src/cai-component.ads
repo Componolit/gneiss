2c4e6b9a4fd3810a70b6e6a13cf42e8df9c71df9
Johannes Kliemann <kliemann@componolit.com>
2019-04-10 18:14:10 +0200

    API comments and minor spec refinements

--- src/cai-component.ads
+++ src/cai-component.ads
@@ -1,8 +1,22 @@
+--
+--  @summary Component construction interface
+--  @author  Johannes Kliemann
+--  @date    2019-04-10
+--
+--  Copyright (C) 2019 Componolit GmbH
+--
+--  This file is part of ada-interface, which is distributed under the terms of the
+--  GNU Affero General Public License version 3.
+--
 
 with Cai.Types;
 
 generic
    with procedure Construct (Cap : Cai.Types.Capability);
+   --  Component initialization procedure, only called once
+   --  only source of an initialized Capability object
+   --
+   --  @param Cap  System capability
 package Cai.Component is
 
    pragma Elaborate_Body;
--- src/cai-log.ads
+++ src/cai-log.ads
@@ -1,3 +1,13 @@
+--
+--  @summary Log interface declarations
+--  @author  Johannes Kliemann
+--  @date    2019-04-10
+--
+--  Copyright (C) 2019 Componolit GmbH
+--
+--  This file is part of ada-interface, which is distributed under the terms of the
+--  GNU Affero General Public License version 3.
+--
 
 private with Cai.Internal.Log;
 
@@ -6,6 +16,7 @@ package Cai.Log with
 is
 
    type Unsigned is mod 2 ** 64;
+   --  Catch all mod type, only used for image functions
 
    function Image (V : Integer) return String with
       Post => Image'Result'Length <= 20 and Image'Result'First = 1;
@@ -17,8 +28,10 @@ is
       Post => Image'Result'Length <= 16 and Image'Result'First = 1;
    function Image (V : Duration) return String with
       Post => Image'Result'Length <= 27 and Image'Result'First = 1;
+   --  Image functions for basic types
 
    type Client_Session is limited private;
+   --  Log client session object
 
 private
 
