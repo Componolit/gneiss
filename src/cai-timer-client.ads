9abea1a8fac4446e4ad46e030505442049600f77
Johannes Kliemann <kliemann@componolit.com>
2019-04-17 13:30:03 +0200

    timer interfaces

---
+++ src/cai-timer-client.ads
@@ -0,0 +1,19 @@
+with Cai.Types;
+
+package Cai.Timer.Client is
+
+   function Create return Client_Session;
+
+   function Initialized (C : Client_Session) return Boolean;
+
+   procedure Initialize (C : in out Client_Session; Cap : Cai.Types.Capability) with
+      Pre => not Initialized (C);
+
+   function Clock (C : Client_Session) return Time with
+      Pre => Initialized (C);
+
+   procedure Finalize (C : in out Client_Session) with
+      Pre  => Initialized (C),
+      Post => not Initialized (C);
+
+end Cai.Timer.Client;
