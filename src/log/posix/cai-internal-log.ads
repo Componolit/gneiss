fafaf22583e6782ec5bf78220e932457ca2d19aa
Johannes Kliemann <kliemann@componolit.com>
2019-04-25 14:35:53 +0200

    fix log newline behaviour on posix

--- src/log/posix/cai-internal-log.ads
+++ src/log/posix/cai-internal-log.ads
@@ -5,6 +5,7 @@ package Cai.Internal.Log is
    type Client_Session is limited record
       Label          : System.Address;
       Length         : Integer;
+      Prev_Nl        : Boolean;
    end record;
 
 end Cai.Internal.Log;
