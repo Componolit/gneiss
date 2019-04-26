df93778512802466dffa78367486d3b95eecd4ef
Johannes Kliemann <kliemann@componolit.com>
2019-04-26 15:00:45 +0200

    inotify events for configuration ref #18

--- src/configuration/client/posix/configuration_client.c
+++ src/configuration/client/posix/configuration_client.c
@@ -6,16 +6,39 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/mman.h>
+#include <sys/inotify.h>
 #include <capability.h>
 
-typedef struct session
+typedef struct session session_t;
+
+struct session
 {
     int fd;
     void *map;
     uint64_t size;
-} session_t;
+    int ifd;
+    void (*load)(session_t*);
+    capability_t *cap;
+};
 
-void configuration_client_initialize(session_t *session, capability_t *cap)
+void set_zero(session_t *session)
+{
+    session->fd = -1;
+    session->map = 0;
+    session->size = 0;
+    session->ifd = -1;
+    session->load = 0;
+    session->cap = 0;
+}
+
+void handle_change(int fd, void *session)
+{
+    struct inotify_event ie;
+    read(fd, &ie, sizeof(struct inotify_event));
+    ((session_t *)session)->load((session_t *)session);
+}
+
+void configuration_client_initialize(session_t *session, capability_t *cap, void (*load)(session_t *))
 {
     struct stat st;
     if(cap->config_file){
@@ -26,15 +49,30 @@ void configuration_client_initialize(session_t *session, capability_t *cap)
                 if(session->map == MAP_FAILED || session->map == 0){
                     perror("map failed");
                     close(session->fd);
-                    session->fd = -1;
-                    session->map = 0;
+                    set_zero(session);
                 }else{
-                    session->size = st.st_size;
+                    session->ifd = inotify_init();
+                    if(session->ifd > -1){
+                        if(inotify_add_watch(session->ifd, cap->config_file, IN_MODIFY) > -1){
+                            session->size = st.st_size;
+                            session->load = load;
+                            session->cap = cap;
+                            cap->enlist(session->ifd, &handle_change, session);
+                        }else{
+                            perror("watch failed");
+                            close(session->fd);
+                            set_zero(session);
+                        }
+                    }else{
+                        perror("inotify failed");
+                        close(session->fd);
+                        set_zero(session);
+                    }
                 }
             }else{
                 perror("stat failed");
                 close(session->fd);
-                session->fd = -1;
+                session->map = 0;
             }
         }else{
             perror(cap->config_file);
@@ -44,9 +82,8 @@ void configuration_client_initialize(session_t *session, capability_t *cap)
 
 void configuration_client_finalize(session_t *session)
 {
+    session->cap->withdraw(session->ifd);
     munmap(session->map, session->size);
     close(session->fd);
-    session->fd = -1;
-    session->map = 0;
-    session->size = 0;
+    set_zero(session);
 }
