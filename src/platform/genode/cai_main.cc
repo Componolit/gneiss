98451a437be6a985ffb25bc0acacacc1151bef8a
Johannes Kliemann <kliemann@componolit.com>
2019-04-26 16:22:37 +0200

    genode component exit ref #34

--- src/platform/genode/cai_main.cc
+++ src/platform/genode/cai_main.cc
@@ -5,6 +5,7 @@ Genode::Env *__genode_env; // only required for ada-runtime
 
 extern "C" void adainit();
 extern "C" void cai_component_construct(Genode::Env *);
+extern "C" void adafinal();
 
 void Component::construct(Genode::Env &env)
 {
@@ -12,4 +13,12 @@ void Component::construct(Genode::Env &env)
     env.exec_static_constructors();
     adainit();
     cai_component_construct(&env);
+}
+
+extern "C" {
+    void cai_component_exit(Genode::Env *env, int status)
+    {
+        adafinal();
+        env->parent().exit(status);
+    }
 }
