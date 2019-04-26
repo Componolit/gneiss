b55c27b6e6c5f2e38655fe2343dbc89656401871
Johannes Kliemann <kliemann@componolit.com>
2019-04-26 16:22:37 +0200

    genode component exit ref #34

--- src/platform/genode/cai-component.adb
+++ src/platform/genode/cai-component.adb
@@ -4,6 +4,22 @@ package body Cai.Component is
    pragma Warnings (Off, "all instances of");
    --  This generic must only be instantiated once
 
+   procedure Shutdown (Cap    : Cai.Types.Capability;
+                       Status : Shutdown_Status)
+   is
+      procedure Genode_Exit (C : Cai.Types.Capability;
+                             S : Integer) with
+         Import,
+         Convention => CPP,
+         External_Name => "cai_component_exit";
+   begin
+      if Status = Success then
+         Genode_Exit (Cap, 0);
+      else
+         Genode_Exit (Cap, 1);
+      end if;
+   end Shutdown;
+
    procedure Platform_Construct (Cap : Cai.Types.Capability) with
       Export,
       Convention    => C,
