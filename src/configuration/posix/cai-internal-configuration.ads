780619f31aaca33290c62e10e903b21b7d24acb0
Johannes Kliemann <kliemann@componolit.com>
2019-04-30 14:01:03 +0200

    fix open and map behviour ref #18

--- src/configuration/posix/cai-internal-configuration.ads
+++ src/configuration/posix/cai-internal-configuration.ads
@@ -1,16 +1,12 @@
 
-with C;
 with System;
 
 package Cai.Internal.Configuration is
 
    type Client_Session is limited record
-      Fd   : Integer;
-      Map  : System.Address;
-      Size : C.Uint64_T;
-      Ifd  : Integer;
-      Load : System.Address;
-      Cap  : System.Address;
+      Ifd   : Integer;
+      Parse : System.Address;
+      Cap   : System.Address;
    end record;
 
 end Cai.Internal.Configuration;
