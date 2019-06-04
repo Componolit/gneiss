0cd735a7fb0da1218cdf79168ef14f2633197ad5
Johannes Kliemann <kliemann@componolit.com>
2019-06-04 16:28:56 +0200

    add basic muen support

---
+++ src/platform/muen/componolit-interfaces-internal-types.ads
@@ -0,0 +1,6 @@
+
+package Componolit.Interfaces.Internal.Types is
+
+   type Capability is new Integer;
+
+end Componolit.Interfaces.Internal.Types;
---
+++ src/platform/muen/componolit-interfaces-internal.ads
@@ -0,0 +1,6 @@
+
+package Componolit.Interfaces.Internal with
+   SPARK_Mode
+is
+
+end Componolit.Interfaces.Internal;
