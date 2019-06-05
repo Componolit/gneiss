a2d486e31bca27ba368b26d889c7d9f76ca6901f
Johannes Kliemann <kliemann@componolit.com>
2019-06-05 12:57:37 +0200

    add log session base for muen

---
+++ src/log/muen/componolit-interfaces-internal-log.ads
@@ -0,0 +1,10 @@
+
+with Componolit.Interfaces.Muen;
+
+package Componolit.Interfaces.Internal.Log with
+   SPARK_Mode
+is
+
+   type Client_Session is new Componolit.Interfaces.Muen.Session_Index;
+
+end Componolit.Interfaces.Internal.Log;
--- src/platform/muen/componolit-interfaces-muen.ads
+++ src/platform/muen/componolit-interfaces-muen.ads
@@ -3,6 +3,21 @@ package Componolit.Interfaces.Muen with
    SPARK_Mode
 is
 
+   type Session_Type is (None, Log);
+
+   type Session_Element (Session : Session_Type := None) is record
+      case Session is
+         when None =>
+            null;
+         when Log =>
+            null;
+      end case;
+   end record;
+
+   type Session_Index is new Natural range 1 .. 256;
+
+   type Session_List is array (Session_Index range <>) of Session_Element;
+
    procedure Main;
 
 end Componolit.Interfaces.Muen;
