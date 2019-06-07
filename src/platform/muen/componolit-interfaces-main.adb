e2baa189c9f2535fa8b7d8355db64000022fcf49
Johannes Kliemann <kliemann@componolit.com>
2019-06-07 13:06:16 +0200

    set component exit status fixes #59

--- src/platform/muen/componolit-interfaces-main.adb
+++ src/platform/muen/componolit-interfaces-main.adb
@@ -3,6 +3,7 @@ with Component;
 with SK.CPU;
 with Componolit.Interfaces.Types;
 with Componolit.Interfaces.Internal.Types;
+with Componolit.Interfaces.Muen;
 
 package body Componolit.Interfaces.Main with
    SPARK_Mode
@@ -11,11 +12,16 @@ is
    procedure Run with
       SPARK_Mode
    is
+      package CIM renames Componolit.Interfaces.Muen;
+      use type CIM.Status;
       Null_Cap : constant Componolit.Interfaces.Internal.Types.Capability := (null record);
       function Gen_Cap is new Ada.Unchecked_Conversion (Componolit.Interfaces.Internal.Types.Capability,
                                                         Componolit.Interfaces.Types.Capability);
    begin
       Component.Main.Construct (Gen_Cap (Null_Cap));
+      while CIM.Component_Status = CIM.Running loop
+         null;
+      end loop;
       SK.CPU.Stop;
    end Run;
 
