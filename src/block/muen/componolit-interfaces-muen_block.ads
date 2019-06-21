b404484e1be71c1d1111ac36d919febe93a2fb53
Johannes Kliemann <kliemann@componolit.com>
2019-06-21 11:51:40 +0200

    block client request sending (wip)

--- src/block/muen/componolit-interfaces-muen_block.ads
+++ src/block/muen/componolit-interfaces-muen_block.ads
@@ -1,4 +1,5 @@
 
+with Ada.Unchecked_Conversion;
 with Interfaces;
 with Componolit.Interfaces.Muchannel_Writer;
 with Componolit.Interfaces.Muchannel_Reader;
@@ -11,14 +12,15 @@ is
    type Count is range 0 .. 2 ** 63 - 1 with
       Size => 64;
 
+   type Sector is range 0 .. 2 ** 63 - 1 with
+      Size => 64;
+
    type Session_Name is new String (1 .. 59);
    Null_Name : constant Session_Name := (others => Character'First);
 
-   type Event_Data_Type is array (1 .. Event_Block_Size) of Standard.Interfaces.Unsigned_8;
-
-   type Command_Type is (Sync, Size) with
-      Size => 64;
-   for Command_Type use (Sync => 0, Size => 1);
+   --  The sector field is used to determine the command type if the event is a command
+   Sync : constant Sector := 0;
+   Size : constant Sector := 1;
 
    type Event_Type is (Read,
                        Write,
@@ -30,12 +32,27 @@ is
                        Command => 2,
                        Last    => 3);
 
+   type Padding_Type is array (Natural range <>) of Standard.Interfaces.Unsigned_8;
+
+   type Raw_Data_Type is array (1 .. Event_Block_Size) of Standard.Interfaces.Unsigned_8 with
+      Size => 32768;
+
+   type Size_Command_Data_Type is record
+      Value : Count;
+      Pad   : Padding_Type (1 .. 4088);
+   end record;
+
+   for Size_Command_Data_Type use record
+      Value at 0 range 0 .. 63;
+      Pad   at 8 range 0 .. 32703;
+   end record;
+
    type Event is record
       Kind  : Event_Type;
       Error : Integer;
-      Id    : Command_Type;
+      Id    : Sector;
       Priv  : Standard.Interfaces.Unsigned_64;
-      Data  : Event_Data_Type;
+      Data  : Raw_Data_Type;
    end record;
 
    for Event use record
@@ -52,16 +69,20 @@ is
                                    Priv  => 0,
                                    Data  => (others => 0));
 
+   Element_Count : constant Positive := 16#0010_0000# / (Event'Size / 8);
+
    package Request_Channel is new Componolit.Interfaces.Muchannel_Writer
       (Element_Type => Event,
-       Elements     => 16#0010_0000# / (Event'Size / 8),
+       Elements     => Element_Count,
        Null_Element => Null_Event,
        Protocol     => 16#9570_208d_ca77_db19#);
 
    package Response_Channel is new Componolit.Interfaces.Muchannel_Reader
       (Element_Type => Event,
-       Elements     => 16#0010_0000# / (Event'Size / 8),
+       Elements     => Element_Count,
        Null_Element => Null_Event,
        Protocol     => 16#9851_be32_82fe_f0dc#);
+
+   function Get_Size_Command_Data is new Ada.Unchecked_Conversion (Raw_Data_Type, Size_Command_Data_Type);
 
 end Componolit.Interfaces.Muen_Block;
