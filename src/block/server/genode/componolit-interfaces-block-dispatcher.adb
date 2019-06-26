5aee6bee1a7224ee18450aec72b5ee64eb4a958c
Johannes Kliemann <kliemann@componolit.com>
2019-06-26 11:06:42 +0200

    use dispatcher capability ref #50

--- src/block/server/genode/componolit-interfaces-block-dispatcher.adb
+++ src/block/server/genode/componolit-interfaces-block-dispatcher.adb
@@ -47,55 +47,63 @@ is
    end Register;
 
    procedure Session_Request (D     : in out Dispatcher_Session;
+                              Cap   :        Dispatcher_Capability;
                               Valid :    out Boolean;
                               Label :    out String;
                               Last  :    out Natural)
    is
-      Label_Address : constant System.Address := Cxx.Block.Dispatcher.Label_Content (D.Instance);
-      Label_Length  : constant Natural        := Natural (Cxx.Block.Dispatcher.Label_Length (D.Instance));
+      Label_Address : constant System.Address :=
+         Cxx.Block.Dispatcher.Label_Content (D.Instance, Cap.Instance);
+      Label_Length  : Natural;
    begin
       Valid := False;
       Label := (others => Character'Val (0));
       Last  := 0;
-      if
-         Label_Address /= System.Null_Address
-         and Label_Length <= Label'Length
-      then
-         declare
-            Lbl : String (1 .. Label_Length)
-            with Address => Label_Address;
-         begin
-            Valid                                               := True;
-            Label (Label'First .. Label'First + Lbl'Length - 1) := Lbl;
-            Last                                                := Label'First + Lbl'Length - 1;
-         end;
+      if Label_Address /= System.Null_Address then
+         Label_Length := Natural (Cxx.Block.Dispatcher.Label_Length (D.Instance, Cap.Instance));
+         if
+            Label_Length <= Label'Length
+         then
+            declare
+               Lbl : String (1 .. Label_Length)
+               with Address => Label_Address;
+            begin
+               Valid                                               := True;
+               Label (Label'First .. Label'First + Lbl'Length - 1) := Lbl;
+               Last                                                := Label'First + Lbl'Length - 1;
+            end;
+         end if;
       end if;
    end Session_Request;
 
    procedure Session_Accept (D : in out Dispatcher_Session;
+                             C :        Dispatcher_Capability;
                              I : in out Server_Session;
                              L :        String) with
       SPARK_Mode => Off
    is
    begin
-      Serv.Initialize (Serv.Instance (I), L, Byte_Length (Cxx.Block.Dispatcher.Session_Size (D.Instance)));
+      Serv.Initialize (Serv.Instance (I),
+                       L,
+                       Byte_Length (Cxx.Block.Dispatcher.Session_Size (D.Instance, C.Instance)));
       Cxx.Block.Server.Initialize (I.Instance,
                                    Cxx.Block.Dispatcher.Get_Capability (D.Instance),
-                                   Cxx.Block.Dispatcher.Session_Size (D.Instance),
+                                   Cxx.Block.Dispatcher.Session_Size (D.Instance, C.Instance),
                                    Serv.Event'Address,
                                    Serv.Block_Count'Address,
                                    Serv.Block_Size'Address,
                                    Serv.Maximum_Transfer_Size'Address,
                                    Serv.Writable'Address);
-      Cxx.Block.Dispatcher.Session_Accept (D.Instance, I.Instance);
+      Cxx.Block.Dispatcher.Session_Accept (D.Instance, C.Instance, I.Instance);
    end Session_Accept;
 
    procedure Session_Cleanup (D : in out Dispatcher_Session;
+                              C :        Dispatcher_Capability;
                               I : in out Server_Session)
    is
    begin
       if
-         Cxx.Block.Dispatcher.Session_Cleanup (D.Instance, I.Instance) = Cxx.Bool'Val (1)
+         Cxx.Block.Dispatcher.Session_Cleanup (D.Instance, C.Instance, I.Instance) = Cxx.Bool'Val (1)
       then
          Serv.Finalize (Serv.Instance (I));
          Cxx.Block.Server.Finalize (I.Instance);
