e7539414b0818d42d699dd1a9706c64e4c98426d
Johannes Kliemann <kliemann@componolit.com>
2019-07-01 14:39:30 +0200

    linux timer timeout ref #67

--- src/timer/client/componolit-interfaces-timer-client.ads
+++ src/timer/client/componolit-interfaces-timer-client.ads
@@ -1,5 +1,7 @@
 with Componolit.Interfaces.Types;
 
+generic
+   with procedure Event;
 package Componolit.Interfaces.Timer.Client with
    SPARK_Mode
 is
@@ -32,6 +34,22 @@ is
    function Clock (C : Client_Session) return Time with
       Volatile_Function,
       Pre => Initialized (C);
+
+   --  Sets the timeout after which the Event procedure will be called
+   --
+   --  The event procedure is called once after the time specified in this procedure.
+   --  The precision of the actual timeout is platform dependent but the time is
+   --  guaranteed to be AT LEAST the time specified in D.
+   --  Only one timeout can be active at a time, if this procedure is called multiple times
+   --  only the last call will be handled.
+   --
+   --  @param C  Timer client session object
+   --  @param D  Timeout event duration
+   procedure Set_Timeout (C : in out Client_Session;
+                          D :        Duration) with
+      Pre  => Initialized (C)
+              and then D > 0.0,
+      Post => Initialized (C);
 
    --  Finalizes timer session
    --
--- src/timer/client/linux/componolit-interfaces-timer-client.adb
+++ src/timer/client/linux/componolit-interfaces-timer-client.adb
@@ -1,4 +1,6 @@
 
+with System;
+
 package body Componolit.Interfaces.Timer.Client with
    SPARK_Mode => Off
 is
@@ -6,20 +8,26 @@ is
    function Create return Client_Session
    is
    begin
-      return False;
+      return (Instance => System.Null_Address);
    end Create;
 
    function Initialized (C : Client_Session) return Boolean
    is
+      use type System.Address;
    begin
-      return Boolean (C);
+      return C.Instance /= System.Null_Address;
    end Initialized;
 
    procedure Initialize (C : in out Client_Session; Cap : Componolit.Interfaces.Types.Capability)
    is
-      pragma Unreferenced (Cap);
+      procedure C_Initialize (Session    : in out System.Address;
+                              Capability :        Componolit.Interfaces.Types.Capability;
+                              Callback   :        System.Address) with
+         Import,
+         Convention => C,
+         External_Name => "timer_client_initialize";
    begin
-      C := True;
+      C_Initialize (C.Instance, Cap, Event'Address);
    end Initialize;
 
    function Clock (C : Client_Session) return Time
@@ -34,10 +42,26 @@ is
       return C_Clock;
    end Clock;
 
+   procedure Set_Timeout (C : in out Client_Session;
+                          D :        Duration)
+   is
+      procedure C_Timeout (Session : System.Address;
+                           Dur     : Duration) with
+         Import,
+         Convention => C,
+         External_Name => "timer_client_set_timeout";
+   begin
+      C_Timeout (C.Instance, D);
+   end Set_Timeout;
+
    procedure Finalize (C : in out Client_Session)
    is
+      procedure C_Finalize (Session : in out System.Address) with
+         Import,
+         Convention => C,
+         External_Name => "timer_client_finalize";
    begin
-      C := False;
+      C_Finalize (C.Instance);
    end Finalize;
 
 end Componolit.Interfaces.Timer.Client;
