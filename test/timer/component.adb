95d504634002b5ba2425ffca88a66b4b7194e31d
Johannes Kliemann <kliemann@componolit.com>
2019-07-01 14:42:03 +0200

    adapt timer test ref #67

--- test/timer/component.adb
+++ test/timer/component.adb
@@ -8,36 +8,57 @@ package body Component with
    SPARK_Mode
 is
 
-   Log : Componolit.Interfaces.Log.Client_Session := Componolit.Interfaces.Log.Client.Create;
-   Timer : Componolit.Interfaces.Timer.Client_Session := Componolit.Interfaces.Timer.Client.Create;
+   procedure Event;
+
+   package Timer_Client is new Componolit.Interfaces.Timer.Client (Event);
+
+   Log        : Componolit.Interfaces.Log.Client_Session   := Componolit.Interfaces.Log.Client.Create;
+   Timer      : Componolit.Interfaces.Timer.Client_Session := Timer_Client.Create;
+   Capability : Componolit.Interfaces.Types.Capability;
 
    procedure Construct (Cap : Componolit.Interfaces.Types.Capability)
    is
    begin
+      Capability := Cap;
       if not Componolit.Interfaces.Log.Client.Initialized (Log) then
          Componolit.Interfaces.Log.Client.Initialize (Log, Cap, "Timer");
       end if;
-      if not Componolit.Interfaces.Timer.Client.Initialized (Timer) then
-         Componolit.Interfaces.Timer.Client.Initialize (Timer, Cap);
+      if not Timer_Client.Initialized (Timer) then
+         Timer_Client.Initialize (Timer, Cap);
       end if;
       if
          Componolit.Interfaces.Log.Client.Initialized (Log)
-         and Componolit.Interfaces.Timer.Client.Initialized (Timer)
+         and Timer_Client.Initialized (Timer)
       then
-         Main.Vacate (Cap, Main.Success);
+         Timer_Client.Set_Timeout (Timer, 60.0);
+         Timer_Client.Set_Timeout (Timer, 1.5);
          Componolit.Interfaces.Log.Client.Info
-            (Log, "Time: " & Componolit.Interfaces.Log.Image
-                                (Duration (Componolit.Interfaces.Timer.Client.Clock (Timer))));
+            (Log, "Time: " & Componolit.Interfaces.Log.Image (Duration (Timer_Client.Clock (Timer))));
       else
          Main.Vacate (Cap, Main.Failure);
       end if;
    end Construct;
 
+   procedure Event
+   is
+   begin
+      if
+         Componolit.Interfaces.Log.Client.Initialized (Log)
+         and Timer_Client.Initialized (Timer)
+      then
+         Componolit.Interfaces.Log.Client.Info
+            (Log, "Time: " & Componolit.Interfaces.Log.Image (Duration (Timer_Client.Clock (Timer))));
+         Main.Vacate (Capability, Main.Success);
+      else
+         Main.Vacate (Capability, Main.Failure);
+      end if;
+   end Event;
+
    procedure Destruct
    is
    begin
-      if Componolit.Interfaces.Timer.Client.Initialized (Timer) then
-         Componolit.Interfaces.Timer.Client.Finalize (Timer);
+      if Timer_Client.Initialized (Timer) then
+         Timer_Client.Finalize (Timer);
       end if;
       if Componolit.Interfaces.Log.Client.Initialized (Log) then
          Componolit.Interfaces.Log.Client.Finalize (Log);
