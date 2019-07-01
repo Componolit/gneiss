7f4e28be1e4f854e4b72a7e11f4670b2e70502d7
Johannes Kliemann <kliemann@componolit.com>
2019-07-01 14:39:30 +0200

    linux timer timeout ref #67

--- src/timer/linux/componolit-interfaces-internal-timer.ads
+++ src/timer/linux/componolit-interfaces-internal-timer.ads
@@ -1,6 +1,10 @@
 
+with System;
+
 package Componolit.Interfaces.Internal.Timer is
 
-   type Client_Session is new Boolean;
+   type Client_Session is limited record
+      Instance : System.Address;
+   end record;
 
 end Componolit.Interfaces.Internal.Timer;
