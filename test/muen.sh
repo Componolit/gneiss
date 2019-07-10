8a300837e111327f8511fdf493388d75e3a9734f
Johannes Kliemann <kliemann@componolit.com>
2019-07-10 17:03:51 +0200

    add muen test script ref #70

---
+++ test/muen.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /muen
+git reset --hard
+git checkout cai
+git submodule update --init components/spark_runtime/src
+rm -r components/cai/src
+ln -fs /ada-interfaces components/cai/src
+make -j$(nproc) SPARK_WARNINGS=continue
+make -j$(nproc) SPARK_WARNINGS=continue COMPONENTS=spark_runtime
+set -e
+make -j$(nproc) SPARK_WARNINGS=continue RTS_DIR=/home/jk/workspace/muen/components/spark_runtime/obj/ AGGREGATE=cai/ada_interface.gpr COMPONENTS_BUILD="sdump"
