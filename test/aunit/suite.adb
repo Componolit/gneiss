3443d90d5918a0860485d009fff9bb20adeef92d
Johannes Kliemann <kliemann@componolit.com>
2019-07-25 16:35:58 +0200

    add unit tests for image functions ref #52

---
+++ test/aunit/suite.adb
@@ -0,0 +1,18 @@
+
+with Componolit.Interfaces.Strings.Tests;
+
+package body Suite
+is
+
+   Result : aliased Aunit.Test_Suites.Test_Suite;
+
+   Strings_Case : aliased Componolit.Interfaces.Strings.Tests.Test_Case;
+
+   function Suite return Aunit.Test_Suites.Access_Test_Suite
+   is
+   begin
+      Result.Add_Test (Strings_Case'Access);
+      return Result'Access;
+   end Suite;
+
+end Suite;
---
+++ test/aunit/suite.ads
@@ -0,0 +1,9 @@
+
+with Aunit.Test_Suites;
+
+package Suite
+is
+
+   function Suite return Aunit.Test_Suites.Access_Test_Suite;
+
+end Suite;
