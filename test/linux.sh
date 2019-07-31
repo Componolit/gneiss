7100ce85f9f8a258115b35ace5b958c171e19a31
Johannes Kliemann <kliemann@componolit.com>
2019-07-31 14:38:03 +0200

    enable linux tests (w/o proof)

--- test/linux.sh
+++ test/linux.sh
@@ -3,31 +3,35 @@
 cd /ada-interfaces
 git submodule update --init --recursive
 make -C ada-runtime && make -C ada-runtime platform
-set -e
 
 gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -u componolit-interfaces-log
 gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux --clean
 gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=hello_world -u component
 gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=hello_world -u component --clean
-#gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=block_client -u component
-#gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=block_client -u component --clean
+gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=block_client -u component
+gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=block_client -u component --clean
 gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=timer -u component
 gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=timer -u component --clean
 gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=rom -u component
 gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=rom -u component --clean
 
-#gprbuild -P ada_interface.gpr -XPLATFORM=linux
-#gprclean -P ada_interface.gpr -XPLATFORM=linux
-#gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=hello_world
-#./build/hello_world
-#gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=hello_world
-#dd if=/dev/zero of=/tmp/test_disk.img bs=4k count=64
-#gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=block_client
-#./build/block_client
-#gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=block_client
-#gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=timer
-#./build/timer
-#gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=timer
-#gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=rom
-#./build/rom test/rom/cai.conf & (sleep 2 && touch test/rom/cai.conf)
-#gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=rom
+set -e
+
+gprbuild -P ada_interface.gpr -XPLATFORM=linux
+gprclean -P ada_interface.gpr -XPLATFORM=linux
+gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=hello_world
+./build/hello_world
+gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=hello_world
+dd if=/dev/zero of=/tmp/test_disk.img bs=4k count=64
+gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=block_client
+./build/block_client
+gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=block_client
+gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=timer
+./build/timer
+gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=timer
+gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=rom
+./build/rom test/rom/cai.conf & (sleep 2 && touch test/rom/cai.conf)
+gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=rom
+
+gprbuild -P test/aunit/test.gpr
+./test/aunit/test
