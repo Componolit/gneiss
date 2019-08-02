384c283124802b6b00a0180da7f87860dcc2a18c
Johannes Kliemann <kliemann@componolit.com>
2019-08-02 16:36:08 +0200

    test cai tests on muen

--- test/muen.sh
+++ test/muen.sh
@@ -2,6 +2,7 @@
 
 cd /muen
 git reset --hard
+git fetch --all
 git checkout cai
 git submodule update --init components/spark_runtime/src
 rm -r components/cai/src
@@ -9,4 +10,4 @@ ln -fs /ada-interfaces components/cai/src
 make -j$(nproc) SPARK_WARNINGS=continue
 make -j$(nproc) SPARK_WARNINGS=continue COMPONENTS=spark_runtime
 set -e
-make -j$(nproc) SPARK_WARNINGS=continue RTS_DIR=/muen/components/spark_runtime/obj/ AGGREGATE=cai/ada_interface.gpr COMPONENTS_BUILD="sdump"
+make -j$(nproc) SPARK_WARNINGS=continue RTS_DIR=/muen/components/spark_runtime/obj/ AGGREGATE=cai/ada_interface.gpr COMPONENTS_BUILD="sdump cai_hello_world cai_timer cai_rom cai_block_client cai_block_server"
