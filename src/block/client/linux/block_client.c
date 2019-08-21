ca01674833a5c49ada0bffd5107768a21eac5c45
Johannes Kliemann <kliemann@componolit.com>
2019-08-21 17:16:33 +0200

    remove instance in linux
    ref #84

--- src/block/client/linux/block_client.c
+++ src/block/client/linux/block_client.c
@@ -61,69 +61,59 @@ void block_client_update_request(block_client_t *client, request_t *request)
     }
 }
 
-void block_client_initialize(block_client_t **client,
+void block_client_initialize(block_client_t *client,
                              const char *path,
                              uint64_t buffer_size,
                              void (*event)(void),
-                             void (*rw)(block_client_t *, uint64_t, request_t const *, void*))
+                             void (*rw)(block_client_t *, request_t const *, void*))
 {
     struct stat st;
-    *client = malloc(sizeof(block_client_t));
-    if(*client){
-        // try to open the file read-write
-        // also no support for symlinks to prevent handling special cases
-        (*client)->fd = open(path, O_RDWR | O_NOFOLLOW);
-        (*client)->writable = 1;
-        if((*client)->fd < 0){
-            // if read-write fails try to open it read only
-            (*client)->fd = open(path, O_RDONLY | O_NOFOLLOW);
-            (*client)->writable = 0;
-        }
-        if((*client)->fd >= 0){
-            if(!fstat((*client)->fd, &st)){
-                if(S_ISREG(st.st_mode)){
-                    (*client)->block_size = st.st_blksize;
-                    (*client)->block_count = st.st_size / st.st_blksize;
-                }else if(S_ISBLK(st.st_mode)){
-                    size_t bytes;
-                    size_t max_blocks;
-                    int ro;
-                    // these are some ioctls from /usr/include/linux/fs.h
-                    // this code most probably only works on linux
-                    ioctl((*client)->fd, _IO(0x12,104), &((*client)->block_size)); // block device sector size
-                    ioctl((*client)->fd, _IOR(0x12,114,size_t), &bytes); // number of bytes
-                    // writability needs to be checked on the block device itself as device files are always rw
-                    ioctl((*client)->fd, _IO(0x12,94), &ro); // read only
-                    (*client)->block_count = bytes / (*client)->block_size;
-                    (*client)->writable = !ro;
-                }else{
-                    fprintf(stderr, "Unsupported file type: 0%o\n", st.st_mode >> 12);
-                    block_client_finalize(client);
-                }
+    // try to open the file read-write
+    // also no support for symlinks to prevent handling special cases
+    client->fd = open(path, O_RDWR | O_NOFOLLOW);
+    client->writable = 1;
+    if(client->fd < 0){
+        // if read-write fails try to open it read only
+        client->fd = open(path, O_RDONLY | O_NOFOLLOW);
+        client->writable = 0;
+    }
+    if(client->fd >= 0){
+        if(!fstat(client->fd, &st)){
+            if(S_ISREG(st.st_mode)){
+                client->block_size = st.st_blksize;
+                client->block_count = st.st_size / st.st_blksize;
+            }else if(S_ISBLK(st.st_mode)){
+                size_t bytes;
+                size_t max_blocks;
+                int ro;
+                // these are some ioctls from /usr/include/linux/fs.h
+                // this code most probably only works on linux
+                ioctl(client->fd, _IO(0x12,104), &(client->block_size)); // block device sector size
+                ioctl(client->fd, _IOR(0x12,114,size_t), &bytes); // number of bytes
+                // writability needs to be checked on the block device itself as device files are always rw
+                ioctl(client->fd, _IO(0x12,94), &ro); // read only
+                client->block_count = bytes / client->block_size;
+                client->writable = !ro;
             }else{
-                perror("failed to get device meta data");
+                fprintf(stderr, "Unsupported file type: 0%o\n", st.st_mode >> 12);
                 block_client_finalize(client);
             }
-            (*client)->event = event;
-            (*client)->rw = rw;
         }else{
-            perror(path);
+            perror("failed to get device meta data");
             block_client_finalize(client);
         }
+        client->event = event;
+        client->rw = rw;
     }else{
-        perror("failed to initialize block client");
-        if(*client){
-            block_client_finalize(client);
-        }
+        perror(path);
+        block_client_finalize(client);
     }
 }
 
-void block_client_finalize(block_client_t **client)
+void block_client_finalize(block_client_t *client)
 {
-    aio_cancel((*client)->fd, 0);
-    close((*client)->fd);
-    free(*client);
-    *client = 0;
+    aio_cancel(client->fd, 0);
+    close(client->fd);
 }
 
 void block_client_enqueue(block_client_t *client, request_t *request)
@@ -131,7 +121,7 @@ void block_client_enqueue(block_client_t *client, request_t *request)
     int status;
     switch(request->kind){
         case CAI_BLOCK_WRITE:
-            client->rw(client, block_client_block_size (client), request, (void *)(request->aio_cb->aio_buf));
+            client->rw(client, request, (void *)(request->aio_cb->aio_buf));
             status = aio_write(request->aio_cb);
             break;
         case CAI_BLOCK_READ:
@@ -154,7 +144,7 @@ void block_client_submit(block_client_t *client)
 
 void block_client_read(block_client_t *client, const request_t *request)
 {
-    client->rw(client, block_client_block_size (client), request, (void *)(request->aio_cb->aio_buf));
+    client->rw(client, request, (void *)(request->aio_cb->aio_buf));
 }
 
 void block_client_release(block_client_t *client, request_t *request)
