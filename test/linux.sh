1b08114fb46cf5e6a60f824e87a5147d626bd2cc
Johannes Kliemann <kliemann@componolit.com>
2019-08-22 16:36:28 +0200

    rename test and build infrastructure
    ref #88

--- test/linux.sh
+++ test/linux.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 
-cd /ada-interfaces
+cd /gneiss
 git submodule update --init --recursive
 make -C ada-runtime && make -C ada-runtime platform
 
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -u componolit-interfaces-log
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux --clean
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=hello_world -u component
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=hello_world -u component --clean
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=block_client -u component
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=block_client -u component --clean
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=timer -u component
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=timer -u component --clean
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=rom -u component
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=rom -u component --clean
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -u componolit-interfaces-log
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux --clean
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=hello_world -u component
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=hello_world -u component --clean
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=block_client -u component
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=block_client -u component --clean
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=timer -u component
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=timer -u component --clean
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=rom -u component
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=rom -u component --clean
 
 set -e
 
-gprbuild -P ada_interface.gpr -XPLATFORM=linux
-gprclean -P ada_interface.gpr -XPLATFORM=linux
-gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=hello_world
+gprbuild -P gneiss.gpr -XPLATFORM=linux
+gprclean -P gneiss.gpr -XPLATFORM=linux
+gprbuild -P gneiss.gpr -XPLATFORM=linux -XTEST=hello_world
 ./build/hello_world
-gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=hello_world
+gprclean -P gneiss.gpr -XPLATFORM=linux -XTEST=hello_world
 dd if=/dev/zero of=/tmp/test_disk.img bs=4k count=64
-gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=block_client
+gprbuild -P gneiss.gpr -XPLATFORM=linux -XTEST=block_client
 ./build/block_client
-gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=block_client
-gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=timer
+gprclean -P gneiss.gpr -XPLATFORM=linux -XTEST=block_client
+gprbuild -P gneiss.gpr -XPLATFORM=linux -XTEST=timer
 ./build/timer
-gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=timer
-gprbuild -P ada_interface.gpr -XPLATFORM=linux -XTEST=rom
+gprclean -P gneiss.gpr -XPLATFORM=linux -XTEST=timer
+gprbuild -P gneiss.gpr -XPLATFORM=linux -XTEST=rom
 ./build/rom test/rom/cai.conf & (sleep 2 && touch test/rom/cai.conf)
-gprclean -P ada_interface.gpr -XPLATFORM=linux -XTEST=rom
+gprclean -P gneiss.gpr -XPLATFORM=linux -XTEST=rom
 
 gprbuild -P test/aunit/test.gpr
 ./test/aunit/test
--- test/muen.sh
+++ test/muen.sh
@@ -5,9 +5,9 @@ git reset --hard
 git fetch --all
 git checkout cai
 git submodule update --init components/spark_runtime/src
-rm -r components/cai/src
-ln -fs /ada-interfaces components/cai/src
+rm -r components/gneiss/src
+ln -fs /gneiss components/gneiss/src
 make -j$(nproc) SPARK_WARNINGS=continue
 make -j$(nproc) SPARK_WARNINGS=continue COMPONENTS=spark_runtime
 set -e
-make -j$(nproc) SPARK_WARNINGS=continue RTS_DIR=/muen/components/spark_runtime/obj/ AGGREGATE=cai/ada_interface.gpr COMPONENTS_BUILD="sdump cai_hello_world cai_timer cai_block_client cai_block_server cai_test cai_rom"
+make -j$(nproc) SPARK_WARNINGS=continue RTS_DIR=/muen/components/spark_runtime/obj/ AGGREGATE=gneiss/gneiss.gpr COMPONENTS_BUILD="sdump cai_hello_world cai_timer cai_block_client cai_block_server cai_block_proxy cai_test cai_rom"
