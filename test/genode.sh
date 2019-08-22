191fc1a7171e996ddffa9e4da5aaa02785a0e582
Johannes Kliemann <kliemann@componolit.com>
2019-08-22 16:36:28 +0200

    rename test and build infrastructure
    ref #88

--- test/genode.sh
+++ test/genode.sh
@@ -1,38 +1,23 @@
 #!/bin/sh
 
-cd /ada-interfaces
-git submodule update --init --recursive
-make -C ada-runtime && make -C ada-runtime platform
-
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=genode -u componolit-interfaces-log
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=genode --clean
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=genode -XTEST=hello_world -u component
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=genode -XTEST=hello_world -u component --clean
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=genode -XTEST=block_client -u component
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=genode -XTEST=block_client -u component --clean
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=genode -XTEST=timer -u component
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=genode -XTEST=timer -u component --clean
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=genode -XTEST=rom -u component
-gnatprove -P ada_interface.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=genode -XTEST=rom -u component --clean
-
 set -e
 cd /genode
 git remote add jklmnn https://github.com/jklmnn/genode.git
 git fetch jklmnn
 git checkout b0d01103e20981d64a81b8494359c9606017e4bf
-git clone https://github.com/Componolit/ada-components.git /genode/repos/ada-components
-cd /genode/repos/ada-components
-git checkout 4763f55770dcc777b713a33a7e16c414ced29af8
-rm -r /genode/repos/ada-components/libs/ada-interface
-ln -fs /ada-interfaces /genode/repos/ada-components/libs/ada-interface
+git clone https://github.com/Componolit/genode-componolit.git /genode/repos/componolit
+cd /genode/repos/componolit
+git checkout 08c98ffe48f6b4899e735d6af742f24543641ef0
+rm -rf /genode/repos/componolit/modules/gneiss
+ln -fs /gneiss /genode/repos/componolit/modules/gneiss
 /genode/tool/create_builddir x86_64
 cd /genode/build/x86_64
 sed -i "s/^#REPOS/REPOS/g;s/^#MAKE.*$/MAKE += -j$(nproc)/g" etc/build.conf
-echo 'REPOSITORIES += $(GENODE_DIR)/repos/ada-components/platform/genode' >> etc/build.conf
+echo 'REPOSITORIES += $(GENODE_DIR)/repos/componolit' >> etc/build.conf
 /genode/tool/ports/prepare_port ada-runtime
-make KERNEL=linux BOARD=linux run/log_hello_world
-make KERNEL=linux BOARD=linux run/configuration
-make KERNEL=linux BOARD=linux run/timer_clock
-make KERNEL=linux BOARD=linux run/block_client
-make KERNEL=linux BOARD=linux run/block_server
-make KERNEL=linux BOARD=linux run/block_proxy
+make KERNEL=linux BOARD=linux run/gneiss/hello_world
+make KERNEL=linux BOARD=linux run/gneiss/rom
+make KERNEL=linux BOARD=linux run/gneiss/timer
+make KERNEL=linux BOARD=linux run/gneiss/block_client
+make KERNEL=linux BOARD=linux run/gneiss/block_server
+make KERNEL=linux BOARD=linux run/gneiss/block_proxy
