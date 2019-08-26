aad2620b0ff4f3859b775da49fb1aab2aeea6550
Johannes Kliemann <kliemann@componolit.com>
2019-08-26 13:34:12 +0200

    prove rom test
    ref #80

--- src/rom/client/muen/componolit-gneiss-rom-client.adb
+++ src/rom/client/muen/componolit-gneiss-rom-client.adb
@@ -18,16 +18,20 @@ is
       Rom_Name : constant Musinfo.Name_Type :=
          (if Name = "" then CIM.String_To_Name ("config") else CIM.String_To_Name (Name));
    begin
-      C.Mem := Musinfo.Instance.Memory_By_Name (Rom_Name);
+      if Musinfo.Instance.Is_Valid then
+         C.Mem := Musinfo.Instance.Memory_By_Name (Rom_Name);
+      end if;
    end Initialize;
 
    procedure Load (C : in out Client_Session)
    is
       use type Standard.Interfaces.Unsigned_64;
       function Max_Index (Size : Standard.Interfaces.Unsigned_64) return Index is
-         (if Standard.Interfaces.Unsigned_64 (Index'Last) > Size then Index (Size) else Index'Last);
+         (if Standard.Interfaces.Unsigned_64 (Index'Last - Index'First) > Size
+          and then Standard.Interfaces.Unsigned_64 (Index'Last) <= Size then Index (Size) else Index'Last);
       I : constant Index := Max_Index (C.Mem.Size / (Element'Size / 8));
       B : Buffer (Index'First .. Index'First + I - 1) with
+         Import,
          Address => System'To_Address (C.Mem.Address);
    begin
       Parse (B);
