da82bc1b2c473b70003be7d4da9a7333b058f70d
Johannes Kliemann <kliemann@componolit.com>
2019-09-18 14:51:17 +0200

    fix hang on too many requests

--- test/block_server/component.adb
+++ test/block_server/component.adb
@@ -121,50 +121,55 @@ is
 
    procedure Event
    is
+      Finished : Boolean := False;
    begin
       if
          Initialized (Server)
          and then Block.Initialized (Server)
       then
-         for I in Request_Cache'Range loop
-            pragma Loop_Invariant (Initialized (Server));
-            pragma Loop_Invariant (Block.Initialized (Server));
-            if Block_Server.Status (Request_Cache (I).Req) = Block.Raw then
-               Request_Cache (I).Success := False;
-               Request_Cache (I).Handled := False;
-               Block_Server.Process (Server, Request_Cache (I).Req);
-            end if;
-            if
-               Block_Server.Status (Request_Cache (I).Req) = Block.Pending
-               and then Block_Server.Assigned (Server, Request_Cache (I).Req)
-               and then not Request_Cache (I).Handled
-            then
-               Request_Cache (I).Handled := True;
-               if
-                  Block_Server.Start (Request_Cache (I).Req) <= Block.Id (Ram_Disk'Length / Disk_Block_Size)
-                  and then Block_Server.Length (Request_Cache (I).Req) > 0
-                  and then Block_Server.Length (Request_Cache (I).Req) <=
-                     Block.Count (Ram_Disk'Length / Disk_Block_Size)
-               then
-                  case Block_Server.Kind (Request_Cache (I).Req) is
-                     when Block.Read =>
-                        Read (Request_Cache (I));
-                     when Block.Write =>
-                        Write (Request_Cache (I));
-                     when others => null;
-                  end case;
-               else
+         loop
+            for I in Request_Cache'Range loop
+               pragma Loop_Invariant (Initialized (Server));
+               pragma Loop_Invariant (Block.Initialized (Server));
+               if Block_Server.Status (Request_Cache (I).Req) = Block.Raw then
                   Request_Cache (I).Success := False;
+                  Request_Cache (I).Handled := False;
+                  Block_Server.Process (Server, Request_Cache (I).Req);
+                  Finished := Block_Server.Status (Request_Cache (I).Req) = Block.Raw;
                end if;
-            end if;
-            if
-               Block_Server.Status (Request_Cache (I).Req) = Block.Pending
-               and then Block_Server.Assigned (Server, Request_Cache (I).Req)
-               and then Request_Cache (I).Handled
-            then
-               Block_Server.Acknowledge (Server, Request_Cache (I).Req,
-                                         (if Request_Cache (I).Success then Block.Ok else Block.Error));
-            end if;
+               if
+                  Block_Server.Status (Request_Cache (I).Req) = Block.Pending
+                  and then Block_Server.Assigned (Server, Request_Cache (I).Req)
+                  and then not Request_Cache (I).Handled
+               then
+                  Request_Cache (I).Handled := True;
+                  if
+                     Block_Server.Start (Request_Cache (I).Req) <= Block.Id (Ram_Disk'Length / Disk_Block_Size)
+                     and then Block_Server.Length (Request_Cache (I).Req) > 0
+                     and then Block_Server.Length (Request_Cache (I).Req) <=
+                        Block.Count (Ram_Disk'Length / Disk_Block_Size)
+                  then
+                     case Block_Server.Kind (Request_Cache (I).Req) is
+                        when Block.Read =>
+                           Read (Request_Cache (I));
+                        when Block.Write =>
+                           Write (Request_Cache (I));
+                        when others => null;
+                     end case;
+                  else
+                     Request_Cache (I).Success := False;
+                  end if;
+               end if;
+               if
+                  Block_Server.Status (Request_Cache (I).Req) = Block.Pending
+                  and then Block_Server.Assigned (Server, Request_Cache (I).Req)
+                  and then Request_Cache (I).Handled
+               then
+                  Block_Server.Acknowledge (Server, Request_Cache (I).Req,
+                                            (if Request_Cache (I).Success then Block.Ok else Block.Error));
+               end if;
+            end loop;
+            exit when Finished;
          end loop;
          Block_Server.Unblock_Client (Server);
       end if;
