4e8660df936d33e32dcc5dca3767040e74a11407
Johannes Kliemann <kliemann@componolit.com>
2019-11-05 15:59:10 +0100

    read resources, link descriptors, setup queues
    ref #107

--- src/platform/linux/init.c
+++ src/platform/linux/init.c
@@ -8,6 +8,7 @@
 #include <list.h>
 #include <component.h>
 #include <config.h>
+#include <resources.h>
 
 #define ENABLE_TRACE
 #include <trace.h>
@@ -17,10 +18,32 @@ static list_t component_registry;
 static list_t resource_registry;
 static component_t component;
 
+static int allocate_resources(list_t *item, unsigned size, void *arg)
+{
+    resource_t *res = (resource_t *)((*item)->content);
+    return allocate_resource(res);
+}
+
+static int find_resources(list_t *item, unsigned size, void *arg)
+{
+    resource_t *res = (resource_t *)((*item)->content);
+    resource_descriptor_t *resd = (resource_descriptor_t *)arg;
+    if(!strcmp(res->type, resd->type) && !strcmp(res->name, resd->name)){
+        setup_resource(res, resd);
+    }
+}
+
+static int setup_resources(list_t *item, unsigned size, void *arg)
+{
+    resource_descriptor_t *res = (resource_descriptor_t *)((*item)->content);
+    list_foreach(resource_registry, &find_resources, res);
+}
+
 static int start_component(list_t *item, unsigned size, void *arg)
 {
     void *handle;
     memcpy(&component, (*item)->content, sizeof(component_t));
+    list_foreach(component.resources, &setup_resources, 0);
     pid_t pid = fork();
     switch(pid){
         case 0:
@@ -53,18 +76,26 @@ int main(int argc, char *argv[])
     }
 
     component_registry = list_new();
+    resource_registry = list_new();
     config = read_config(argv[1]);
+    TRACE("parse_resources\n");
     status = parse_resources(config, resource_registry);
     if(status){
         fprintf(stderr, "failed to parse resources\n");
         return status;
     }
+    TRACE("parse_components\n");
     status = parse_components(config, component_registry);
     if(status){
         fprintf(stderr, "failed to parse components\n");
         return status;
     }
+    TRACE("allocate_resources %u\n", list_length(resource_registry));
+    if(list_foreach(resource_registry, &allocate_resources, 0)){
+        fprintf(stderr, "failed to allocate resources\n");
+        return 1;
+    }
+    TRACE("start_component %u\n", list_length(component_registry));
     list_foreach(component_registry, &start_component, 0);
-
     return 0;
 }
