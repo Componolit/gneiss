71b020de5f0e7bb7660c27967222646a0160be76
Johannes Kliemann <kliemann@componolit.com>
2019-11-05 15:59:10 +0100

    read resources, link descriptors, setup queues
    ref #107

--- src/platform/linux/config.c
+++ src/platform/linux/config.c
@@ -16,12 +16,11 @@ int parse_resources(xmlNode *root, list_t resources)
     char *value;
     for(xmlNode *node = root->children; node; node = node->next){
         memset(&res, 0, sizeof(res));
-        if(strcmp(node->name, "service")){
+        if(strcmp(node->name, "resource")){
             continue;
         }
         res.type = xmlGetProp(node, "type");
         res.name = xmlGetProp(node, "name");
-        res.fd = -1;
         if(!res.name || !res.type){
             fprintf(stderr, "resource without name or type\n");
             return 1;
@@ -32,6 +31,7 @@ int parse_resources(xmlNode *root, list_t resources)
         res.write = value ? atoi(value) : 0;
         value = xmlGetProp(node, "read_write");
         res.read_write = value ? atoi(value) : 0;
+        TRACE("add resource %s %s\n", res.type, res.name);
         list_append(resources, (void *)&res, sizeof(res));
     }
     return 0;
@@ -40,6 +40,8 @@ int parse_resources(xmlNode *root, list_t resources)
 int parse_components(xmlNode *root, list_t components)
 {
     component_t comp;
+    resource_descriptor_t resd;
+    char *mode;
     for(xmlNode *node = root->children; node; node = node->next){
         memset(&comp, 0, sizeof(comp));
         if(strcmp(node->name, "component")){
@@ -61,6 +63,36 @@ int parse_components(xmlNode *root, list_t components)
             strcat(comp.file, "libcomponent_");
             strcat(comp.file, comp.name);
             strcat(comp.file, ".so");
+        }
+        comp.resources = list_new();
+        for(xmlNode *res = node->children; res; res = res->next){
+            memset(&resd, 0, sizeof(resd));
+            mode = 0;
+            if(strcmp(res->name, "resource")){
+                continue;
+            }
+            resd.type = xmlGetProp(res, "type");
+            resd.name = xmlGetProp(res, "name");
+            resd.label = xmlGetProp(res, "label");
+            mode = xmlGetProp(res, "mode");
+            if(!resd.type || !resd.name || !resd.label || !mode){
+                fprintf(stderr, "ignoring invalid resource\n");
+                continue;
+            }
+            if(!strcmp(mode, "read")){
+                resd.mode = RESOURCE_READ;
+            }else if(!strcmp(mode, "write")){
+                resd.mode = RESOURCE_WRITE;
+            }else if(!strcmp(mode, "read_write")){
+                resd.mode = RESOURCE_READ_WRITE;
+            }else{
+                fprintf(stderr, "resource %s of %s has invalid mode %s\n",
+                        resd.name, comp.name, mode);
+                continue;
+            }
+            TRACE("add resource desc %s %s %s %s\n", resd.type, resd.name, resd.label, mode);
+            resd.fd = -1;
+            list_append(comp.resources, (void *)&resd, sizeof(resd));
         }
         list_append(components, (void *)&comp, sizeof(comp));
     }
---
+++ src/platform/linux/resources.h
@@ -0,0 +1,10 @@
+
+#ifndef _GNEISS_RESOURCES_H_
+#define _GNEISS_RESOURCES_H_
+
+#include <component.h>
+
+int allocate_resource(resource_t *resource);
+int setup_resource(resource_t *resource, resource_descriptor_t *resource_descriptor);
+
+#endif /* ifndef _GNEISS_RESOURCES_H_ */
