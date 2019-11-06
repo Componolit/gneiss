92e93fc8c4045306eb3a956ad0af36ce00d2bbb6
Johannes Kliemann <kliemann@componolit.com>
2019-11-06 12:31:41 +0100

    fix resource descriptor finding
    ref #107

--- src/platform/linux/componolit-gneiss-platform.adb
+++ src/platform/linux/componolit-gneiss-platform.adb
@@ -1,8 +1,6 @@
 
 with Ada.Unchecked_Conversion;
 with Componolit.Gneiss.Internal.Types;
-with Componolit.Runtime.Debug;
-with System;
 
 package body Componolit.Gneiss.Platform with
    SPARK_Mode => Off
@@ -13,7 +11,6 @@ is
    procedure Set_Status (C : Gns.Types.Capability;
                          S : Integer)
    is
-      use type System.Address;
       procedure Set (Cp : Gns.Types.Capability;
                      St : Integer) with
          Import,
@@ -21,5 +18,26 @@ is
    begin
       Set (C, S);
    end Set_Status;
+
+   procedure Find_Resource (C :     Gns.Types.Capability;
+                            T :     String;
+                            N :     String;
+                            M :     Integer;
+                            E :     System.Address;
+                            D : out Resource_Descriptor)
+   is
+      procedure Find_Res (Cap    :     Gns.Types.Capability;
+                          R_Type :     System.Address;
+                          Name   :     System.Address;
+                          Mode   :     Integer;
+                          Event  :     System.Address;
+                          Res    : out Resource_Descriptor) with
+         Import,
+         Address => Convert (C).Find_Resource;
+      C_Type : String := T & Character'First;
+      C_Name : String := N & Character'First;
+   begin
+      Find_Res (C, C_Type'Address, C_Name'Address, M, E, D);
+   end Find_Resource;
 
 end Componolit.Gneiss.Platform;
