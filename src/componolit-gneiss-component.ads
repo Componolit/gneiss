4f3d992fcf2e78a40218cfbe1713519655eed228
Johannes Kliemann <kliemann@componolit.com>
2019-11-07 10:18:39 +0100

    emply 79 character lines for component spec

--- src/componolit-gneiss-component.ads
+++ src/componolit-gneiss-component.ads
@@ -5,8 +5,8 @@
 --
 --  Copyright (C) 2019 Componolit GmbH
 --
---  This file is part of Gneiss, which is distributed under the terms of the
---  GNU Affero General Public License version 3.
+--  This file is part of Gneiss, which is distributed under the
+--  terms of the GNU Affero General Public License version 3.
 --
 
 with Componolit.Gneiss.Types;
@@ -15,10 +15,12 @@ generic
    --  Component intialization procedure
    --
    --  @param Cap  Capability provided by the platform to use services
-   with procedure Component_Construct (Capability : Componolit.Gneiss.Types.Capability);
+   with procedure Component_Construct
+      (Capability : Componolit.Gneiss.Types.Capability);
 
    --  Component destruction procedure
-   --  This procedure is called after Vacate has been called and the procedure Vacate has been called from returned.
+   --  This procedure is called after Vacate has been called and the
+   --  procedure Vacate has been called from returned.
    with procedure Component_Destruct;
 package Componolit.Gneiss.Component with
    SPARK_Mode,
