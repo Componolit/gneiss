87572d7cedebb45f64f14b389ac2787fdde414f4
Johannes Kliemann <kliemann@componolit.com>
2019-11-11 17:09:16 +0100

    linux log session with message interface
    ref #109

--- src/log/linux/componolit-gneiss-log.adb
+++ src/log/linux/componolit-gneiss-log.adb
@@ -1,15 +1,14 @@
 
-with System;
+with Componolit.Gneiss.Message;
 
 package body Componolit.Gneiss.Log with
    SPARK_Mode
 is
-   use type System.Address;
 
    function Initialized (C : Client_Session) return Boolean is
-      (C.Label /= System.Null_Address);
+      (Componolit.Gneiss.Message.Initialized (C.Session));
 
    function Maximum_Message_Length (C : Client_Session) return Integer is
-      (4095);
+      (C.Buffer'Length);
 
 end Componolit.Gneiss.Log;
