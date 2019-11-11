bf28f47cb9c59bbc81f6d7f6368eca7daeefd251
Johannes Kliemann <kliemann@componolit.com>
2019-11-11 17:31:47 +0100

    change linux tests to new init
    ref #107

--- test/linux.sh
+++ test/linux.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
+apt update
+apt install -y expect
+
 cd /gneiss
 git submodule update --init --recursive
-make -C ada-runtime && make -C ada-runtime platform
+make -C ada-runtime
 
 gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -u componolit-gneiss-log
 gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux --clean
-gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=hello_world -u component
-gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=hello_world -u component --clean
-gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=block_client -u component
-gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=block_client -u component --clean
-gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=timer -u component
-gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=timer -u component --clean
-gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=rom -u component
-gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=rom -u component --clean
 
 set -e
 
@@ -21,22 +16,15 @@ gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -u com
 gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux --clean
 gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=fifo -u component
 gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=fifo -u component --clean
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=hello_world -u component
+gnatprove -P gneiss.gpr -j0 --level=2 --checks-as-errors -XPLATFORM=linux -XTEST=hello_world -u component --clean
 
-gprbuild -P gneiss.gpr -XPLATFORM=linux
-gprclean -P gneiss.gpr -XPLATFORM=linux
+gprbuild -P gneiss.gpr -XPLATFORM=linux -XTEST=init
 gprbuild -P gneiss.gpr -XPLATFORM=linux -XTEST=hello_world
-./build/hello_world
-gprclean -P gneiss.gpr -XPLATFORM=linux -XTEST=hello_world
-dd if=/dev/zero of=/tmp/test_disk.img bs=4k count=64
-gprbuild -P gneiss.gpr -XPLATFORM=linux -XTEST=block_client
-./build/block_client
-gprclean -P gneiss.gpr -XPLATFORM=linux -XTEST=block_client
-gprbuild -P gneiss.gpr -XPLATFORM=linux -XTEST=timer
-./build/timer
-gprclean -P gneiss.gpr -XPLATFORM=linux -XTEST=timer
-gprbuild -P gneiss.gpr -XPLATFORM=linux -XTEST=rom
-./build/rom test/rom/cai.conf & (sleep 2 && touch test/rom/cai.conf)
-gprclean -P gneiss.gpr -XPLATFORM=linux -XTEST=rom
+gprbuild -P gneiss.gpr -XPLATFORM=linux -XTEST=message_server
+
+export LD_LIBRARY_PATH=build/libcomponents:ada-runtime/obj/adalib
+expect test/hello_world/hello_world.expect
 
 gprbuild -P test/aunit/test.gpr
 ./test/aunit/test
