e9c21db40d9b33151f14ade09153321f24ceede2
Johannes Kliemann <kliemann@componolit.com>
2019-11-12 19:01:31 +0100

    slicer aunit tests
    ref #118

--- test/aunit/suite.adb
+++ test/aunit/suite.adb
@@ -1,6 +1,7 @@
 
 with Componolit.Gneiss.Strings.Tests;
 with Fifo_Tests;
+with Slicer_Tests;
 
 package body Suite
 is
@@ -9,12 +10,14 @@ is
 
    Strings_Case : aliased Componolit.Gneiss.Strings.Tests.Test_Case;
    Fifo_Case    : aliased Fifo_Tests.Test_Case;
+   Slicer_Case  : aliased Slicer_Tests.Test_Case;
 
    function Suite return Aunit.Test_Suites.Access_Test_Suite
    is
    begin
       Result.Add_Test (Strings_Case'Access);
       Result.Add_Test (Fifo_Case'Access);
+      Result.Add_Test (Slicer_Case'Access);
       return Result'Access;
    end Suite;
 
