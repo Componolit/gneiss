90ca7e4ea4e13f3eafc3e28348a739608c6b6110
Johannes Kliemann <kliemann@componolit.com>
2019-11-12 10:51:34 +0100

    add message documentation
    ref #109

--- src/message/componolit-gneiss-message.ads
+++ src/message/componolit-gneiss-message.ads
@@ -1,3 +1,13 @@
+--
+--  @summary Message interface declarations
+--  @author  Johannes Kliemann
+--  @date    2019-11-12
+--
+--  Copyright (C) 2019 Componolit GmbH
+--
+--  This file is part of ada-interface, which is distributed under the terms of the
+--  GNU Affero General Public License version 3.
+--
 
 private with Componolit.Gneiss.Internal.Message;
 
@@ -5,10 +15,20 @@ package Componolit.Gneiss.Message with
    SPARK_Mode
 is
 
+   --  Reader and Writer session objects
    type Writer_Session is limited private;
    type Reader_Session is limited private;
 
+   --  Checks if W is initialized
+   --
+   --  @param W  Writer session instance
+   --  @return   True if W is initialized
    function Initialized (W : Writer_Session) return Boolean;
+
+   --  Checks if R is initialized
+   --
+   --  @param R  Reader session instance
+   --  @return   True if R is initialized
    function Initialized (R : Reader_Session) return Boolean;
 
 private
