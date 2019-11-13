eca8d37523ea21cc566c6db3f7fc4116f2dff3aa
Johannes Kliemann <kliemann@componolit.com>
2019-11-13 10:52:24 +0100

    prove slicer
    ref #118

--- src/common/componolit-gneiss-slicer.adb
+++ src/common/componolit-gneiss-slicer.adb
@@ -3,52 +3,54 @@ package body Componolit.Gneiss.Slicer with
    SPARK_Mode
 is
 
-   function Create (Range_First : Index;
-                    Range_Last  : Index;
-                    Slice       : Index) return Context
+   function Create (Range_First  : Index;
+                    Range_Last   : Index;
+                    Slice_Length : Index) return Context
    is
       Sf : Index;
       Sl : Index;
    begin
-      if Range_Last - Range_First + 1 < Slice then
-         Sf := Range_First;
+      Sf := Range_First;
+      if Range_Last - Range_First + 1 < Slice_Length then
          Sl := Range_Last;
       else
-         Sf := Range_First;
-         Sl := Range_First + Slice - 1;
+         Sl := Range_First + (Slice_Length - 1);
       end if;
-      return Context'(Range_First => Range_First,
-                      Range_Last  => Range_Last,
-                      Slice       => Slice,
-                      Slice_First => Sf,
-                      Slice_Last  => Sl);
+      return Context'(Full_Range   => (First => Range_First,
+                                       Last  => Range_Last),
+                      Slice_Range  => (First => Sf,
+                                       Last  => Sl),
+                      Slice_Length => Slice_Length);
    end Create;
 
-   function First (C : Context) return Index is
-      (C.Slice_First);
+   function Get_Slice (C : Context) return Slice is
+      (C.Slice_Range);
 
-   function Last (C : Context) return Index is
-      (C.Slice_Last);
+   function Get_Range (C : Context) return Slice is
+      (C.Full_Range);
+
+   function Get_Length (C : Context) return Index is
+      (C.Slice_Length);
 
    function Has_Next (C : Context) return Boolean is
-      (C.Slice_Last < C.Range_Last);
+      (C.Slice_Range.Last < C.Full_Range.Last);
 
    procedure Next (C : in out Context)
    is
       Slice_First : Index;
       Slice_Last  : Index;
    begin
-      Slice_First := C.Slice_Last + 1;
-      if C.Range_Last - C.Slice_Last < C.Slice then
-         Slice_Last := C.Range_Last;
+      Slice_First := C.Slice_Range.Last + 1;
+      if C.Full_Range.Last - C.Slice_Range.Last < C.Slice_Length then
+         Slice_Last := C.Full_Range.Last;
       else
-         Slice_Last := C.Slice_Last + C.Slice;
+         Slice_Last := C.Slice_Range.Last + C.Slice_Length;
       end if;
-      C := Context'(Range_First => C.Range_First,
-                    Range_Last  => C.Range_Last,
-                    Slice       => C.Slice,
-                    Slice_First => Slice_First,
-                    Slice_Last  => Slice_Last);
+      C := Context'(Full_Range   => (First => C.Full_Range.First,
+                                     Last  => C.Full_Range.Last),
+                    Slice_Range  => (First => Slice_First,
+                                     Last  => Slice_Last),
+                    Slice_Length => C.Slice_Length);
    end Next;
 
 end Componolit.Gneiss.Slicer;
