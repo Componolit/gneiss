33820e49deab7935876a834e59c5d9f3036ff35b
Johannes Kliemann <kliemann@componolit.com>
2019-11-21 16:08:09 +0100

    update tests
    fixes #123

--- test/muen.sh
+++ test/muen.sh
@@ -3,10 +3,13 @@
 cd /muen
 git reset --hard
 git fetch --all
-git checkout cai
+git checkout fc67ec8bbdb14b0986652a2870ae754e69d9f0b7
 git submodule update --init components/spark_runtime/src
 rm -r components/gneiss/src
 ln -fs /gneiss components/gneiss/src
+cd /muen/components/gneiss/src
+git submodule update --init lib/basalt
+cd -
 make -j$(nproc) SPARK_WARNINGS=continue
 make -j$(nproc) SPARK_WARNINGS=continue COMPONENTS=spark_runtime
 set -e
