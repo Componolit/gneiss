1bd6d3bdd3332fb640316527fde03bc8e90aed60
Johannes Kliemann <kliemann@componolit.com>
2019-11-29 16:47:55 +0100

    add empty message server implementation
    ref #119

--- src/message/server/gneiss-message-dispatcher.ads
+++ src/message/server/gneiss-message-dispatcher.ads
@@ -1,53 +1,57 @@
 
-with Gneiss.Types.Capability;
+with Gneiss.Types;
 with Gneiss.Message.Server;
 
 generic
+   pragma Warnings (Off, "* is not referenced");
    with package Server_Instance is new Gneiss.Message.Server (<>);
    with procedure Dispatch (Session    : in out Dispatcher_Session;
                             Capability :        Dispatcher_Capability);
+   pragma Warnings (On, "* is not referenced");
 package Gneiss.Message.Dispatcher with
    SPARK_Mode
 is
+   pragma Unevaluated_Use_Of_Old (Allow);
 
    procedure Initialize (Session    : in out Dispatcher_Session;
                          Capability :        Gneiss.Types.Capability);
 
    procedure Register (Session : in out Dispatcher_Session) with
-      Pre  => State (Session) = Initialized,
-      Post => State (Session) = Initialized;
+      Pre  => Status (Session) = Initialized,
+      Post => Status (Session) = Initialized;
 
    procedure Finalize (Session : in out Dispatcher_Session) with
-      Post => State (Session) = Uninitialized;
+      Post => Status (Session) = Uninitialized;
 
    function Valid_Session_Request (Session    : Dispatcher_Session;
                                    Capability : Dispatcher_Capability) return Boolean with
-      Pre => State (Session) = Initialized;
+      Pre => Status (Session) = Initialized;
 
    procedure Session_Initialize (Session    : in out Dispatcher_Session;
                                  Capability :        Dispatcher_Capability;
                                  Server_S   : in out Server_Session) with
-      Pre  => State (Session) = Initialized
+      Pre  => Status (Session) = Initialized
               and then Valid_Session_Request (Session, Capability)
               and then not Server_Instance.Ready (Server_S)
-              and then State (Server_S) = Uninitialized,
-      Post => State (Session) = Initialized
+              and then Status (Server_S) = Uninitialized,
+      Post => Status (Session) = Initialized
               and then Valid_Session_Request (Session, Capability);
 
    procedure Session_Accept (Session    : in out Dispatcher_Session;
                              Capability :        Dispatcher_Capability;
                              Server_S   : in out Server_Session) with
-      Pre  => State (Session) = Initialized
+      Pre  => Status (Session) = Initialized
               and then Valid_Session_Request (Session, Capability)
               and then Server_Instance.Ready (Server_S)
-              and then State (Server_S) = Initialized,
-      Post => State (Session) = Initialized
+              and then Status (Server_S) = Initialized,
+      Post => Status (Session) = Initialized
               and then Server_Instance.Ready (Server_S)
-              and then State (Server_S) = Initialized;
+              and then Status (Server_S) = Initialized;
 
-   procedure Session_Cleanup (Session  : in out Dispatcher_Session;
-                              Server_S : in out Server_Session) with
-      Pre  => State (Session) = Initialized,
-      Post => State (Session) = Initialized;
+   procedure Session_Cleanup (Session    : in out Dispatcher_Session;
+                              Capability :        Dispatcher_Capability;
+                              Server_S   : in out Server_Session) with
+      Pre  => Status (Session) = Initialized,
+      Post => Status (Session) = Initialized;
 
 end Gneiss.Message.Dispatcher;
