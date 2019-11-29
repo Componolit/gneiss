a65e2256f360e8288d34bab7150ee691cc59bcc5
Johannes Kliemann <kliemann@componolit.com>
2019-11-29 14:28:13 +0100

    add package generator
    ref #126

---
+++ src/platform/linux/gneiss-protocoll.adb
@@ -0,0 +1,18 @@
+
+with System;
+with Gneiss.Syscall;
+
+package body Gneiss.Protocoll with
+   SPARK_Mode
+is
+
+   procedure Send_Message (Destination : Integer;
+                           Data        : Message;
+                           File_Desc   : Integer := -1) with
+      SPARK_Mode => Off
+   is
+   begin
+      Gneiss.Syscall.Write_Message (Destination, Data'Address, Data'Size / 8, File_Desc);
+   end Send_Message;
+
+end Gneiss.Protocoll;
