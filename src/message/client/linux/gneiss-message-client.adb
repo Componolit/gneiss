a5131f9319f571a2803a2e0530b7c8aa22cc1e36
Johannes Kliemann <kliemann@componolit.com>
2019-12-02 18:08:50 +0100

    update sxml

--- src/message/client/linux/gneiss-message-client.adb
+++ src/message/client/linux/gneiss-message-client.adb
@@ -5,6 +5,7 @@ with Gneiss.Epoll;
 with Gneiss.Platform;
 with Gneiss.Syscall;
 with Gneiss.Protocoll;
+with Basalt.Strings;
 with Componolit.Runtime.Debug;
 
 package body Gneiss.Message.Client with
@@ -15,12 +16,38 @@ is
    type RFLX_String is array (RFLX.Session.Length_Type range <>) of Character;
    package Proto is new Gneiss.Protocoll (Character, RFLX_String);
 
+   procedure Peek_Message (Fd : Integer);
+
    function Get_Event_Address return System.Address with
       SPARK_Mode => Off
    is
    begin
       return Event'Address;
    end Get_Event_Address;
+
+   Read_Buffer : Proto.Message := (Length      => 255,
+                                   Action      => RFLX.Session.Request,
+                                   Kind        => RFLX.Session.Message,
+                                   Name_Length => 0,
+                                   Payload     => (others => Character'First));
+   Read_String : String (1 .. 255) with
+      Address => Read_Buffer.Payload'Address;
+
+   procedure Peek_Message (Fd : Integer) with
+      SPARK_Mode => Off
+   is
+      Len    : Integer;
+      Trunc  : Integer;
+      New_Fd : Integer;
+   begin
+      Gneiss.Syscall.Peek_Message (Fd, Read_Buffer'Address, Read_Buffer'Size / 8, New_Fd, Len, Trunc);
+      Componolit.Runtime.Debug.Log_Debug ("Peek New_Fd="
+                                          & Basalt.Strings.Image (New_Fd)
+                                          & " Len="
+                                          & Basalt.Strings.Image (Len)
+                                          & " Trunc="
+                                          & Basalt.Strings.Image (Trunc));
+   end Peek_Message;
 
    function Create_Request (Label : RFLX_String) return Proto.Message is
       (Proto.Message'(Length      => Label'Length,
@@ -36,7 +63,7 @@ is
       Success : Integer;
       C_Label : RFLX_String (RFLX.Session.Length_Type (Label'First) .. RFLX.Session.Length_Type (Label'Last));
    begin
-      --  Componolit.Runtime.Debug.Log_Debug ("Initialize " & Label);
+      Componolit.Runtime.Debug.Log_Debug ("Initialize " & Label);
       for I in C_Label'Range loop
          C_Label (I) := Label (Positive (I));
       end loop;
@@ -44,7 +71,9 @@ is
          when Initialized =>
             return;
          when Pending =>
-            return;
+            Peek_Message (Gneiss.Platform.Get_Broker (Capability));
+            Componolit.Runtime.Debug.Log_Debug ("Pending label: " & Read_String);
+            Gneiss.Syscall.Drop_Message (Gneiss.Platform.Get_Broker (Capability));
          when Uninitialized =>
             Gneiss.Syscall.Dup (Gneiss.Platform.Get_Broker (Capability), Session.Broker);
             if Session.Broker < 0 then
