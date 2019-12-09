f5e0975f306ef35259790b422bf433b4d5c2f028
Johannes Kliemann <kliemann@componolit.com>
2019-12-09 16:55:47 +0100

    implement message server
    ref #119

--- src/message/server/linux/gneiss-message-server.adb
+++ src/message/server/linux/gneiss-message-server.adb
@@ -1,22 +1,27 @@
 
+with Gneiss_Internal.Message;
+
 package body Gneiss.Message.Server with
    SPARK_Mode
 is
 
-   function Available (Session : Server_Session) return Boolean is (False);
+   function Available (Session : Server_Session) return Boolean is
+      (Gneiss_Internal.Message.Peek (Session.Fd) >= Message_Buffer'Length);
 
    procedure Write (Session : in out Server_Session;
-                    Data    :        Message_Buffer)
+                    Data    :        Message_Buffer) with
+      SPARK_Mode => Off
    is
    begin
-      null;
+      Gneiss_Internal.Message.Write (Session.Fd, Data'Address, Data'Length);
    end Write;
 
    procedure Read (Session : in out Server_Session;
-                   Data    :    out Message_Buffer)
+                   Data    :    out Message_Buffer) with
+      SPARK_Mode => Off
    is
    begin
-      null;
+      Gneiss_Internal.Message.Read (Session.Fd, Data'Address, Data'Length);
    end Read;
 
 end Gneiss.Message.Server;
