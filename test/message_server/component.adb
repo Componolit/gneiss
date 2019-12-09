624485eb720952cddcab7f3957b23048270c648f
Johannes Kliemann <kliemann@componolit.com>
2019-12-09 17:02:57 +0100

    implement message server test
    ref #119

--- test/message_server/component.adb
+++ test/message_server/component.adb
@@ -1,4 +1,5 @@
 
+with System;
 with Gneiss.Message;
 with Gneiss.Message.Dispatcher;
 with Gneiss.Message.Server;
@@ -11,7 +12,20 @@ is
    type Unsigned_Char is mod 2 ** 8;
    type C_String is array (Positive range <>) of Unsigned_Char;
 
+   procedure Print_Message (Prefix : System.Address;
+                            Msg    : System.Address) with
+      Import,
+      Convention => C,
+      External_Name => "print_message";
+
    package Message is new Gneiss.Message (Positive, Unsigned_Char, C_String, 1, 128);
+
+   type Server_Slot is record
+      Server : Message.Server_Session;
+      Ident  : String (1 .. 513) := (others => ASCII.NUL);
+   end record;
+
+   type Server_Reg is array (Integer range <>) of Server_Slot;
 
    procedure Event;
 
@@ -30,9 +44,10 @@ is
    package Message_Dispatcher is new Message.Dispatcher (Message_Server, Dispatch);
 
    Dispatcher  : Message.Dispatcher_Session;
-   Server      : Message.Server_Session;
    Capability  : Gneiss.Capability;
    Initialized : Boolean := False;
+   Buffer      : C_String (1 .. 129) := (others => 0);
+   Servers     : Server_Reg (1 .. 1);
 
    procedure Construct (Cap : Gns.Capability)
    is
@@ -56,7 +71,15 @@ is
    procedure Event
    is
    begin
-      null;
+      for S of Servers loop
+         if
+            Message.Initialized (S.Server)
+            and then Message_Server.Available (S.Server)
+         then
+            Message_Server.Read (S.Server, Buffer (1 .. 128));
+            Print_Message (S.Ident'Address, Buffer'Address);
+         end if;
+      end loop;
    end Event;
 
    procedure Initialize (Session : in out Message.Server_Session)
@@ -81,12 +104,20 @@ is
    begin
       Componolit.Runtime.Debug.Log_Debug ("Dispatch " & Name & " " & Label);
       if Message_Dispatcher.Valid_Session_Request (Session, Disp_Cap) then
-         Message_Dispatcher.Session_Initialize (Session, Disp_Cap, Server);
-         if Ready (Server) and then Message.Initialized (Server) then
-            Message_Dispatcher.Session_Accept (Session, Disp_Cap, Server);
-         end if;
+         for S of Servers loop
+            if not Ready (S.Server) then
+               Message_Dispatcher.Session_Initialize (Session, Disp_Cap, S.Server);
+               if Ready (S.Server) and then Message.Initialized (S.Server) then
+                  S.Ident (1 .. Name'Length + Label'Length + 1) := Name & ":" & Label;
+                  Message_Dispatcher.Session_Accept (Session, Disp_Cap, S.Server);
+                  exit;
+               end if;
+            end if;
+         end loop;
       end if;
-      Message_Dispatcher.Session_Cleanup (Session, Disp_Cap, Server);
+      for S of Servers loop
+         Message_Dispatcher.Session_Cleanup (Session, Disp_Cap, S.Server);
+      end loop;
    end Dispatch;
 
    function Ready (Session : Message.Server_Session) return Boolean is
