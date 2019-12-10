fec338a3c70c85b84648ac38d07a55ec85012038
Johannes Kliemann <kliemann@componolit.com>
2019-12-10 15:47:07 +0100

    fix component lookup
    ref #126

--- src/platform/linux/epoll.c
+++ src/platform/linux/epoll.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <sys/epoll.h>
 
+#define ENABLE_TRACE
+#include <trace.h>
+
 void gneiss_epoll_create(int *efd)
 {
     *efd = epoll_create1(0);
+    TRACE("efd=%d\n", *efd);
     if(*efd < 0){
         perror("epoll_create1");
     }
@@ -13,6 +17,7 @@ void gneiss_epoll_create(int *efd)
 #define gneiss_epoll_add(name, type) \
     void gneiss_epoll_add_##name(int efd, int fd, type priv, int *success) \
 { \
+    TRACE("efd=%d fd=%d\n", efd, fd); \
     struct epoll_event eev; \
     eev.events = EPOLLRDHUP | EPOLLIN; \
     eev.data.name = priv; \
@@ -27,6 +32,7 @@ gneiss_epoll_add(ptr, void *)
 
 void gneiss_epoll_remove(int efd, int fd, int *success)
 {
+    TRACE("efd=%d fd=%d\n", efd, fd);
     *success = epoll_ctl(efd, EPOLL_CTL_DEL, fd, 0);
     if(*success < 0){
         perror("epoll_ctl");
@@ -36,6 +42,7 @@ void gneiss_epoll_remove(int efd, int fd, int *success)
 #define gneiss_epoll_wait(name, type) \
     void gneiss_epoll_wait_##name(int efd, uint32_t *event, type *priv) \
 { \
+    TRACE("efd=%d\n", efd); \
     struct epoll_event eev; \
     eev.events = 0; \
     eev.data.name = 0; \
