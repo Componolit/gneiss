f8bf3ef253bb9bd961f5f836ba1484af72978964
Johannes Kliemann <kliemann@componolit.com>
2019-12-17 17:54:43 +0100

    specify globals in Gneiss.Main
    ref #126

--- src/platform/linux/gneiss-main.ads
+++ src/platform/linux/gneiss-main.ads
@@ -1,18 +1,28 @@
 
 with RFLX.Types;
+with Gneiss.Syscall;
+with Gneiss_Epoll;
+with Gneiss.Linker;
 
 package Gneiss.Main with
-   SPARK_Mode
+   SPARK_Mode,
+   Abstract_State => Component_State,
+   Initializes    => Component_State
 is
 
-   procedure Run (Name       :     String;
-                  Fd         :     Integer;
-                  Status     : out Integer);
+   procedure Run (Name   :     String;
+                  Fd     :     Integer;
+                  Status : out Integer) with
+      Global => (In_Out => (Component_State,
+                            Gneiss.Syscall.Linux,
+                            Gneiss_Epoll.Linux,
+                            Gneiss.Linker.Linux));
 
    procedure Peek_Message (Socket    :     Integer;
                            Message   : out RFLX.Types.Bytes;
                            Last      : out RFLX.Types.Index;
                            Truncated : out Boolean;
-                           Fd        : out Integer);
+                           Fd        : out Integer) with
+      Global => (In_Out => Gneiss.Syscall.Linux);
 
 end Gneiss.Main;
