3ac44503907f106cab24b72ec5c13ea7022244fd
Johannes Kliemann <kliemann@componolit.com>
2019-12-26 17:39:23 +0100

    check for config in default path /etc/gneiss/config.xml

--- src/platform/linux/main.adb
+++ src/platform/linux/main.adb
@@ -8,19 +8,25 @@ is
    Cap    : Gneiss.Config.Config_Capability;
    Status : Integer;
 begin
-   if Gneiss.Command_Line.Argument_Count = 2 then
-      Gneiss.Config.Load (Gneiss.Command_Line.Argument (1), Cap);
-      if Gneiss.Config.Valid (Cap) then
-         declare
-            Conf : String (1 .. Gneiss.Config.Get_Length (Cap)) with
-               Import,
-               Address => Gneiss.Config.Get_Address (Cap);
-         begin
-            Gneiss.Broker.Construct (Conf, Status);
-            Gneiss.Command_Line.Set_Exit_Status (Status);
-         end;
-      else
+   case Gneiss.Command_Line.Argument_Count is
+      when 1 =>
+         Gneiss.Config.Load ("/etc/gneiss/config.xml", Cap);
+      when 2 =>
+         Gneiss.Config.Load (Gneiss.Command_Line.Argument (1), Cap);
+      when others =>
          Gneiss.Command_Line.Set_Exit_Status (1);
-      end if;
+         return;
+   end case;
+   if Gneiss.Config.Valid (Cap) then
+      declare
+         Conf : String (1 .. Gneiss.Config.Get_Length (Cap)) with
+            Import,
+            Address => Gneiss.Config.Get_Address (Cap);
+      begin
+         Gneiss.Broker.Construct (Conf, Status);
+         Gneiss.Command_Line.Set_Exit_Status (Status);
+      end;
+   else
+      Gneiss.Command_Line.Set_Exit_Status (1);
    end if;
 end Main;
