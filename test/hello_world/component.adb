ebbc0b06dfc35496d49b6812c2845a5ade16ab43
Johannes Kliemann <kliemann@componolit.com>
2020-01-06 16:38:22 +0100

    adapt hello world log test
    ref #127

--- test/hello_world/component.adb
+++ test/hello_world/component.adb
@@ -6,31 +6,42 @@ package body Component with
    SPARK_Mode
 is
 
+   package Log_Client is new Gneiss.Log.Client (Event);
+
+   Cap : Gneiss.Capability;
    Log : Gneiss.Log.Client_Session;
 
-   procedure Construct (Cap : Gneiss.Capability)
+   procedure Construct (Capability : Gneiss.Capability)
    is
    begin
-      if not Gneiss.Log.Initialized (Log) then
-         Gneiss.Log.Client.Initialize (Log, Cap, "log_hello_world");
-      end if;
-      if Gneiss.Log.Initialized (Log) then
-         Main.Vacate (Cap, Main.Success);
-         Gneiss.Log.Client.Info (Log, "Hello World!");
-         Gneiss.Log.Client.Warning (Log, "Hello World!");
-         Gneiss.Log.Client.Error (Log, "Hello World!");
-      else
-         Main.Vacate (Cap, Main.Failure);
-      end if;
+      Cap := Capability;
+      Log_Client.Initialize (Log, Cap, "log_hello_world");
    end Construct;
+
+   procedure Event
+   is
+   begin
+      case Gneiss.Log.Status (Log) is
+         when Gneiss.Initialized =>
+            Main.Vacate (Cap, Main.Success);
+            Log_Client.Info (Log, "Hello World!");
+            Log_Client.Warning (Log, "Hello World!");
+            Log_Client.Error (Log, "Hello World!");
+         when Gneiss.Pending =>
+            Log_Client.Initialize (Log, Cap, "log_hello_world");
+         when Gneiss.Uninitialized =>
+            Main.Vacate (Cap, Main.Failure);
+      end case;
+   end Event;
 
    procedure Destruct
    is
+      use type Gneiss.Session_Status;
    begin
-      if Gneiss.Log.Initialized (Log) then
-         Gneiss.Log.Client.Info (Log, "Destructing...");
-         Gneiss.Log.Client.Finalize (Log);
+      if Gneiss.Log.Status (Log) = Gneiss.Initialized then
+         Log_Client.Info (Log, "Destructing...");
       end if;
+      Log_Client.Finalize (Log);
    end Destruct;
 
 end Component;
