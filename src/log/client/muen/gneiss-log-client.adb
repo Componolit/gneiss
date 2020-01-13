d10325eaa5e2a879e371a986310603f15a446898
Johannes Kliemann <kliemann@componolit.com>
2020-01-13 15:26:26 +0100

    use async log initialization on muen
    ref #125

--- src/log/client/muen/gneiss-log-client.adb
+++ src/log/client/muen/gneiss-log-client.adb
@@ -7,6 +7,7 @@ with Debuglog.Types;
 with Debuglog.Stream;
 with Debuglog.Stream.Writer_Instance;
 with Gneiss.Muen;
+with Gneiss.Muen_Registry;
 
 package body Gneiss.Log.Client with
    SPARK_Mode
@@ -14,6 +15,16 @@ is
    use type Standard.Interfaces.Unsigned_64;
 
    package CIM renames Gneiss.Muen;
+   package Reg renames Gneiss.Muen_Registry;
+
+   function Event_Address return System.Address;
+
+   function Event_Address return System.Address with
+      SPARK_Mode => Off
+   is
+   begin
+      return Event'Address;
+   end Event_Address;
 
    procedure Activate_Channel (Mem : Musinfo.Memregion_Type) with
       Pre => Debuglog.Stream.Channel_Type'Size <= Mem.Size;
@@ -58,7 +69,7 @@ is
 
    procedure Put (C    : in out Client_Session;
                   Char :        Character) with
-      Pre => Initialized (C);
+      Pre => Status (C) = Initialized;
 
    procedure Put (C    : in out Client_Session;
                   Char :        Character)
@@ -74,51 +85,68 @@ is
       end if;
    end Put;
 
-   procedure Put (C   : in out Client_Session;
-                  Str :        String) with
-      Pre => Initialized (C);
-
-   procedure Put (C   : in out Client_Session;
-                  Str :        String)
+   procedure Print (Session : in out Client_Session;
+                    Msg     :        String)
    is
    begin
-      for Char of Str loop
-         Put (C, Char);
+      for C of Msg loop
+         Put (Session, C);
       end loop;
-   end Put;
+   end Print;
 
-   procedure Initialize (C     : in out Client_Session;
-                         Cap   :        Capability;
-                         Label :        String)
+   procedure Initialize (Session : in out Client_Session;
+                         Cap     :        Capability;
+                         Label   :        String;
+                         Idx     :        Session_Index := 0)
    is
       use type Musinfo.Name_Type;
       use type Musinfo.Memregion_Type;
+      use type CIM.Session_Id;
+      use type CIM.Async_Session_Type;
       pragma Unreferenced (Cap);
       Name   : constant Musinfo.Name_Type := CIM.String_To_Name (Label);
       Memory : Musinfo.Memregion_Type;
+      M_Idx  : CIM.Session_Id;
    begin
-      if Initialized (C) then
+      if Status (Session) = Initialized then
          return;
       end if;
+      for I in Reg.Registry'Range loop
+         if Reg.Registry (I).Kind = CIM.None then
+            M_Idx := I;
+            exit;
+         end if;
+      end loop;
       Memory := Musinfo.Instance.Memory_By_Name (Name);
-      if Name /= Musinfo.Null_Name and Memory /= Musinfo.Null_Memregion then
-         C.Name := Name;
-         C.Mem  := Memory;
+      if
+         Name /= Musinfo.Null_Name
+         and then Memory /= Musinfo.Null_Memregion
+         and then M_Idx /= CIM.Invalid_Index
+      then
+         Reg.Registry (M_Idx) := Reg.Session_Entry'(Kind             => CIM.Log_Client,
+                                                    Log_Client_Event => Event_Address);
+         Session.R_Index := M_Idx;
+         Session.Name    := Name;
+         Session.Mem     := Memory;
+         Session.S_Index := Idx;
          Activate_Channel (Memory);
       end if;
    end Initialize;
 
-   procedure Finalize (C : in out Client_Session)
+   procedure Finalize (Session : in out Client_Session)
    is
    begin
-      if not Initialized (C) then
+      if Status (Session) = Uninitialized then
          return;
       end if;
-      Deactivate_Channel (C.Mem);
-      C.Name   := Musinfo.Null_Name;
-      C.Mem    := Musinfo.Null_Memregion;
-      C.Index  := Debuglog.Types.Message_Index'First;
-      C.Buffer := Debuglog.Types.Null_Data;
+      Deactivate_Channel (Session.Mem);
+      Reg.Registry (Session.R_Index) := Reg.Session_Entry'(Kind => CIM.None);
+      Session.R_Index := CIM.Invalid_Index;
+      Session.S_Index := 0;
+      Session.Name    := Musinfo.Null_Name;
+      Session.Mem     := Musinfo.Null_Memregion;
+      Session.Index   := Debuglog.Types.Message_Index'First;
+      Session.Buffer  := Debuglog.Types.Null_Data;
    end Finalize;
 
    function Get_Label (C : Client_Session) return String;
@@ -129,47 +157,47 @@ is
       return "[" & CIM.Name_To_String (C.Name) & "] ";
    end Get_Label;
 
-   procedure Info (C       : in out Client_Session;
+   procedure Info (Session : in out Client_Session;
                    Msg     :        String;
                    Newline :        Boolean := True)
    is
    begin
-      Put (C, Get_Label (C) & "Info: " & Msg);
+      Print (Session, Get_Label (Session) & "Info: " & Msg);
       if Newline then
-         Put (C, Character'Val (10));
+         Put (Session, ASCII.LF);
       end if;
    end Info;
 
-   procedure Warning (C       : in out Client_Session;
+   procedure Warning (Session : in out Client_Session;
                       Msg     :        String;
                       Newline :        Boolean := True)
    is
    begin
-      Put (C, Get_Label (C) & "Warning: " & Msg);
+      Print (Session, Get_Label (Session) & "Warning: " & Msg);
       if Newline then
-         Put (C, Character'Val (10));
+         Put (Session, ASCII.LF);
       end if;
    end Warning;
 
-   procedure Error (C       : in out Client_Session;
+   procedure Error (Session : in out Client_Session;
                     Msg     :        String;
                     Newline :        Boolean := True)
    is
    begin
-      Put (C, Get_Label (C) & "Error: " & Msg);
+      Print (Session, Get_Label (Session) & "Error: " & Msg);
       if Newline then
-         Put (C, Character'Val (10));
+         Put (Session, ASCII.LF);
       end if;
    end Error;
 
-   procedure Flush (C : in out Client_Session)
+   procedure Flush (Session : in out Client_Session)
    is
    begin
-      C.Buffer.Timestamp := Musinfo.Instance.TSC_Schedule_Start;
-      Write_Channel (C.Mem,
-                     C.Buffer);
-      C.Index  := Debuglog.Types.Message_Index'First;
-      C.Buffer := Debuglog.Types.Null_Data;
+      Session.Buffer.Timestamp := Musinfo.Instance.TSC_Schedule_Start;
+      Write_Channel (Session.Mem,
+                     Session.Buffer);
+      Session.Index  := Debuglog.Types.Message_Index'First;
+      Session.Buffer := Debuglog.Types.Null_Data;
    end Flush;
 
 end Gneiss.Log.Client;
