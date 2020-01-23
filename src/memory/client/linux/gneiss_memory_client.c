cd40390a9b0dcf1f0b386ba2db879b2e86468174
Johannes Kliemann <kliemann@componolit.com>
2020-01-23 14:00:31 +0100

    add tracing, fix mmap check
    ref #110

--- src/memory/client/linux/gneiss_memory_client.c
+++ src/memory/client/linux/gneiss_memory_client.c
@@ -5,8 +5,12 @@
 #include <stdio.h>
 #include <sys/mman.h>
 
+//#define ENABLE_TRACE
+#include <trace.h>
+
 int stat_size(int fd)
 {
+    TRACE("fd=%d\n", fd);
     struct stat st;
     if(fstat(fd, &st) < 0){
         perror("fstat");
@@ -17,8 +21,9 @@ int stat_size(int fd)
 
 void map_file(int fd, void **map, int writable)
 {
+    TRACE("fd=%d *map=%p writable=%d\n", fd, *map, writable);
     *map = mmap(0, stat_size(fd), writable ? PROT_READ | PROT_WRITE : PROT_READ, MAP_SHARED, fd, 0);
-    if(*map = MAP_FAILED){
+    if(*map == MAP_FAILED){
         perror("mmap");
         *map = 0x0;
     }
@@ -26,6 +31,7 @@ void map_file(int fd, void **map, int writable)
 
 void unmap_file(int fd, void **map)
 {
+    TRACE("fd=%d *map=%p\n", fd, *map);
     if(munmap(*map, stat_size(fd)) < 0){
         perror("munmap");
     }
