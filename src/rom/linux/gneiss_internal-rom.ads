5c887fdf818b774c28e6fe146cd4d341296c975c
Johannes Kliemann <kliemann@componolit.com>
2020-01-28 16:56:27 +0100

    rename memory session to rom (Linux)
    ref #120

--- src/rom/linux/gneiss_internal-rom.ads
+++ src/rom/linux/gneiss_internal-rom.ads
@@ -2,16 +2,15 @@
 with System;
 with Gneiss;
 
-package Gneiss_Internal.Memory with
+package Gneiss_Internal.Rom with
    SPARK_Mode
 is
 
    type Client_Session is limited record
       Fd       : Integer                     := -1;
-      Writable : Boolean                     := False;
       Index    : Gneiss.Session_Index_Option := Gneiss.Session_Index_Option'(Valid => False);
       Label    : Session_Label;
       Map      : System.Address              := System.Null_Address;
    end record;
 
-end Gneiss_Internal.Memory;
+end Gneiss_Internal.Rom;
