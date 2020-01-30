40901319c67ff7b499ca3c387f693d00bfc4119c
Johannes Kliemann <kliemann@componolit.com>
2020-01-30 16:22:15 +0100

    temporarily disable memory test on linux
    ref #120

--- test/linux.sh
+++ test/linux.sh
@@ -14,11 +14,9 @@ set -e
 ./cement test/hello_world/hello_world.xml . lib test
 ./cement test/log_proxy/log_proxy.xml . lib test
 ./cement test/rom/rom.xml . lib test
-./cement test/memory/memory.xml . lib test
 
 export LD_LIBRARY_PATH=build/lib
 expect test/message_client/message_client.expect
 expect test/hello_world/hello_world.expect
 expect test/log_proxy/log_proxy.expect
 expect test/rom/rom.expect
-expect test/memory/memory.expect
