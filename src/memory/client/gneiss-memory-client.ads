f70e1eca9e2035c1c13cd974eb89fd1da9c51994
Johannes Kliemann <kliemann@componolit.com>
2020-02-05 15:32:36 +0100

    memory spec and genode stub
    ref #120

---
+++ src/memory/client/gneiss-memory-client.ads
@@ -0,0 +1,33 @@
+--
+--  @summary Shared memory client interface declarations
+--  @author  Johannes Kliemann
+--  @date    2020-02-05
+--
+--  Copyright (C) 2020 Componolit GmbH
+--
+--  This file is part of Gneiss, which is distributed under the terms of the
+--  GNU Affero General Public License version 3.
+--
+
+generic
+   pragma Warnings (Off, "* is not referenced");
+   with procedure Event;
+   with procedure Modify (Session : in out Client_Session;
+                          Data    : in out Buffer);
+   pragma Warnings (On, "* is not referenced");
+package Gneiss.Memory.Client with
+   SPARK_Mode
+is
+
+   procedure Initialize (Session : in out Client_Session;
+                         Cap     :        Capability;
+                         Label   :        String;
+                         Size    :        Long_Integer;
+                         Idx     :        Session_Index := 1);
+
+   procedure Update (Session : in out Client_Session) with
+      Pre => Status (Session) = Initialized;
+
+   procedure Finalize (Session : in out Client_Session);
+
+end Gneiss.Memory.Client;
---
+++ src/memory/server/genode/gneiss-memory-server.adb
@@ -0,0 +1,12 @@
+
+package body Gneiss.Memory.Server with
+   SPARK_Mode
+is
+
+   procedure Modify (Session : in out Server_Session)
+   is
+   begin
+      null;
+   end Modify;
+
+end Gneiss.Memory.Server;
