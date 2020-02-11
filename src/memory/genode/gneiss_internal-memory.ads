a1d03dad6dc78615f2483f20e33d3807ca9fdf9d
Johannes Kliemann <kliemann@componolit.com>
2020-02-11 11:07:18 +0100

    Genode memory session
    ref #120

--- src/memory/genode/gneiss_internal-memory.ads
+++ src/memory/genode/gneiss_internal-memory.ads
@@ -1,22 +1,35 @@
 
+with Gneiss;
+with System;
+
 package Gneiss_Internal.Memory with
    SPARK_Mode
 is
 
    type Client_Session is limited record
-      null;
+      Session : System.Address              := System.Null_Address;
+      Index   : Gneiss.Session_Index_Option := Gneiss.Session_Index_Option'(Valid => False);
+      Size    : Long_Integer                := 0;
+      Addr    : System.Address              := System.Null_Address;
    end record;
 
    type Server_Session is limited record
-      null;
+      Component : System.Address              := System.Null_Address;
+      Addr      : System.Address              := System.Null_Address;
+      Size      : Long_Integer                := 0;
+      Index     : Gneiss.Session_Index_Option := Gneiss.Session_Index_Option'(Valid => False);
    end record;
 
    type Dispatcher_Session is limited record
-      null;
+      Root     : System.Address              := System.Null_Address;
+      Env      : System.Address              := System.Null_Address;
+      Index    : Gneiss.Session_Index_Option := Gneiss.Session_Index_Option'(Valid => False);
+      Dispatch : System.Address              := System.Null_Address;
    end record;
 
    type Dispatcher_Capability is limited record
-      null;
+      Session : System.Address;
+      Size    : Long_Integer;
    end record;
 
 end Gneiss_Internal.Memory;
