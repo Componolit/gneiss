ed76e907cc56c8596f938379fc4a73a8a44ebb29
Johannes Kliemann <kliemann@componolit.com>
2020-03-06 14:28:21 +0100

    improved c++ interface for message session
    ref #154

--- src/message/client/genode/gneiss_message_client.cc
+++ src/message/client/genode/gneiss_message_client.cc
@@ -16,7 +16,7 @@ void Gneiss::Message_Client::initialize(Gneiss::Capability *capability, const ch
 {
     TLOG("capability=", capability, " label=", Genode::Cstring(label));
     check_factory(_factory, *(capability->env));
-    _connection = _factory->create2<Gneiss::Message_Connection>(*(capability->env), label, this);
+    _connection = _factory->create2<Gneiss::Message_Connection>(*(capability->env), label, *this);
 }
 
 void Gneiss::Message_Client::finalize()
@@ -43,19 +43,29 @@ int Gneiss::Message_Client::avail()
     return (int)(_connection->avail());
 }
 
-Gneiss::Message_Connection::Message_Connection(Genode::Env &env, Genode::Session_label label, Gneiss::Message_Client *client) :
+Gneiss::Message_Connection::Message_Connection(Genode::Env &env, Genode::Session_label label, Gneiss::Message_Client &client) :
     Genode::Connection<Gneiss::Message_Session>(env, session(env.parent(),
                                                      "ram_quota=%ld, cap_quota=%ld, label=\"%s\"",
                                                      RAM_QUOTA, CAP_QUOTA, label.string())),
     Gneiss::Message_Session_Client(cap()),
     _init_sigh(env.ep(), *this, &Gneiss::Message_Connection::init),
     _event_sigh(env.ep(), *this, &Gneiss::Message_Connection::event),
-    _client(client)
+    _client(&client)
 {
     TLOG("label=", label, " client=", client);
     sigh(_event_sigh);
     Genode::Signal_transmitter(_init_sigh).submit();
 }
+
+Gneiss::Message_Connection::Message_Connection(Genode::Env &env, Genode::Session_label label) :
+    Genode::Connection<Gneiss::Message_Session>(env, session(env.parent(),
+                                                     "ram_quota=%ld, cap_quota=%ld, label=\"%s\"",
+                                                     RAM_QUOTA, CAP_QUOTA, label.string())),
+    Gneiss::Message_Session_Client(cap()),
+    _init_sigh(env.ep(), *this, &Gneiss::Message_Connection::dummy_event),
+    _event_sigh(env.ep(), *this, &Gneiss::Message_Connection::dummy_event),
+    _client(nullptr)
+{ }
 
 void Gneiss::Message_Connection::init()
 {
@@ -68,3 +78,6 @@ void Gneiss::Message_Connection::event()
     TLOG("");
     _client->_event();
 }
+
+void Gneiss::Message_Connection::dummy_event()
+{ }
