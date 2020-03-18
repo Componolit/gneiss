8798be89d4cdfd3148c30c240cbfed9b75b753c6
Johannes Kliemann <kliemann@componolit.com>
2020-03-18 14:52:04 +0100

    remove duplicated code

--- src/platform/linux/gneiss_access.adb
+++ src/platform/linux/gneiss_access.adb
@@ -3,8 +3,34 @@ package body Gneiss_Access with
    SPARK_Mode => Off
 is
 
-   Data : aliased RFLX.Types.Bytes (1 .. Length);
+   Buffer : aliased RFLX.Types.Bytes (1 .. Length);
+
+   procedure Get (Data : RFLX.Types.Bytes)
+   is
+      use type RFLX.Types.Length;
+      I : Natural := Field'First;
+   begin
+      for J in Data'Range loop
+         Field (I) := Character'Val (RFLX.Types.Byte'Pos (Data (J)));
+         exit when I = Field'Last or else J = Data'Last;
+         I := I + 1;
+      end loop;
+      Last := I;
+   end Get;
+
+   procedure Set (Data : out RFLX.Types.Bytes)
+   is
+      use type RFLX.Types.Length;
+      I : Natural := Field'First;
+   begin
+      Data := (others => RFLX.Types.Byte'First);
+      for J in Data'Range loop
+         Data (J) := RFLX.Types.Byte'Val (Character'Pos (Field (I)));
+         exit when I = Field'Last or else J = Data'Last;
+         I := I + 1;
+      end loop;
+   end Set;
 
 begin
-   Ptr := Data'Unrestricted_Access;
+   Ptr := Buffer'Unrestricted_Access;
 end Gneiss_Access;
--- src/platform/linux/gneiss_access.ads
+++ src/platform/linux/gneiss_access.ads
@@ -10,4 +10,13 @@ is
 
    Ptr : RFLX.Types.Bytes_Ptr;
 
+   generic
+      Field : in out String;
+      Last  : in out Natural;
+   procedure Get (Data : RFLX.Types.Bytes);
+
+   generic
+      Field : String;
+   procedure Set (Data : out RFLX.Types.Bytes);
+
 end Gneiss_Access;
