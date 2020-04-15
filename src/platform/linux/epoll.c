01bad58954651da9c490948c407a04ddfeda4793
Johannes Kliemann <kliemann@componolit.com>
2020-04-15 15:56:55 +0200

    improve error messages

--- src/platform/linux/epoll.c
+++ src/platform/linux/epoll.c
@@ -10,7 +10,7 @@ void gneiss_epoll_create(int *efd)
     *efd = epoll_create1(0);
     TRACE("efd=%d\n", *efd);
     if(*efd < 0){
-        warn("");
+        warn("%s: epoll_create1(0)", __func__);
     }
 }
 
@@ -23,7 +23,7 @@ void gneiss_epoll_create(int *efd)
     eev.data.name = priv; \
     *success = epoll_ctl(efd, EPOLL_CTL_ADD, fd, &eev); \
     if(*success < 0){ \
-        warn("efd=%d fd=%d", efd, fd); \
+        warn("%s: epoll_ctl(efd=%d, EPOLL_CTL_ADD, fd=%d, eev=%p)", __func__, efd, fd, &eev); \
     } \
 }
 
@@ -35,7 +35,7 @@ void gneiss_epoll_remove(int efd, int fd, int *success)
     TRACE("efd=%d fd=%d\n", efd, fd);
     *success = epoll_ctl(efd, EPOLL_CTL_DEL, fd, 0);
     if(*success < 0){
-        warn("efd=%d fd=%d", efd, fd);
+        warn("%s: epoll_ctl(efd=%d, EPOLL_CTL_DEL, fd=%d, 0)", __func__, efd, fd);
     }
 }
 
@@ -47,7 +47,7 @@ void gneiss_epoll_remove(int efd, int fd, int *success)
     eev.events = 0; \
     eev.data.name = 0; \
     if(epoll_wait(efd, &eev, 1, -1) < 0){ \
-        warn("efd=%d", efd); \
+        warn("%s: epoll_wait(efd=%d, eev=%p, 1, -1)", efd, &eev); \
     } \
     *priv = eev.data.name; \
     *event = eev.events; \
