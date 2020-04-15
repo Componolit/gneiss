45e8750205e8dd752c6e7ad10f7fd3f7b761c589
Johannes Kliemann <kliemann@componolit.com>
2020-04-15 16:16:33 +0200

    custom parameter for rom client callback
    ref #165

--- test/rom/component.adb
+++ test/rom/component.adb
@@ -9,13 +9,19 @@ package body Component with
 is
 
    package Rom is new Gneiss.Rom (Character, Positive, String);
-   procedure Read (Session : in out Rom.Client_Session;
-                   Data    :        String);
-   package Rom_Client is new Rom.Client (Read);
 
    C      : Gneiss.Capability;
    Log    : Gneiss.Log.Client_Session;
    Config : Rom.Client_Session;
+
+   procedure Read (Session : in out Rom.Client_Session;
+                   Data    :        String;
+                   Ctx     : in out Gneiss.Log.Client_Session) with
+      Pre  => Gneiss.Log.Initialized (Ctx),
+      Post => Gneiss.Log.Initialized (Ctx);
+
+   package Rom_Client is new Rom.Client (Gneiss.Log.Client_Session, Read);
+   procedure Update is new Rom_Client.Update (Gneiss.Log.Initialized);
 
    procedure Construct (Capability : Gneiss.Capability)
    is
@@ -24,14 +30,16 @@ is
       Gneiss.Log.Client.Initialize (Log, C, "rom");
       Rom_Client.Initialize (Config, C, "config");
       if Gneiss.Log.Initialized (Log) and then Rom.Initialized (Config) then
-         Rom_Client.Update (Config);
+         Update (Config, Log);
+         Main.Vacate (C, Main.Success);
       else
          Main.Vacate (C, Main.Failure);
       end if;
    end Construct;
 
    procedure Read (Session : in out Rom.Client_Session;
-                   Data    :        String)
+                   Data    :        String;
+                   Ctx     : in out Gneiss.Log.Client_Session)
    is
       pragma Unreferenced (Session);
       Prefix : constant String := "Rom content: ";
@@ -42,12 +50,7 @@ is
       else
          Last := Data'Last - Prefix'Length;
       end if;
-      if Gneiss.Log.Initialized (Log) then
-         Gneiss.Log.Client.Info (Log, Prefix & Data (Data'First .. Last));
-         Main.Vacate (C, Main.Success);
-      else
-         Main.Vacate (C, Main.Failure);
-      end if;
+      Gneiss.Log.Client.Info (Ctx, Prefix & Data (Data'First .. Last));
    end Read;
 
    procedure Destruct
