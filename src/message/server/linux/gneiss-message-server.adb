4fe8c24b01b53cf2baf038179e9010c7cb8310ec
Johannes Kliemann <kliemann@componolit.com>
2020-04-16 17:11:53 +0200

    custom parameter and proof for message server
    ref #165

--- src/message/server/linux/gneiss-message-server.adb
+++ src/message/server/linux/gneiss-message-server.adb
@@ -6,9 +6,11 @@ package body Gneiss.Message.Server with
 is
 
    procedure Send (Session : in out Server_Session;
-                   Data    :        Message_Buffer) with
+                   Data    :        Message_Buffer;
+                   Ctx     :        Context) with
       SPARK_Mode => Off
    is
+      pragma Unreferenced (Ctx);
    begin
       Gneiss_Internal.Message_Syscall.Write (Session.Fd, Data'Address, Data'Size * 8);
    end Send;
