2bec2125bcf02c4b9f7a24bceedbda353192c42c
Isabell Zorr <zorr@componolit.com>
2020-05-06 17:14:09 +0200

    Make Debug procedure generic
    ref #169

--- src/core/nRF52832/sparkfun-debug.ads
+++ src/core/nRF52832/sparkfun-debug.ads
@@ -1,12 +1,21 @@
-package Sparkfun.Debug is
+package Sparkfun.Debug with
+   SPARK_Mode
+is
 
    procedure Initialize;
-   procedure Debug (Char : Character);
+
+   generic
+      type T is (<>);
+   procedure Debug (Value : T);
 
 private
+
    type Array_Index is range 1 .. 8;
+
    type Pin_Array is array (Array_Index'Range) of GPIO.Pin;
+
    Pins : constant Pin_Array := (24, 23, 22, 20, 12, 11, 13, 14);
+
    type Value_Array is array (Array_Index'Range) of GPIO.Value;
 
 end Sparkfun.Debug;
