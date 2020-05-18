333584bc07c15957b4740082d8b7d030ded440c4
Johannes Kliemann <kliemann@componolit.com>
2020-05-18 17:34:44 +0200

    Implement basic packet echo server
    ref #108

--- test/packet_client/component.adb
+++ test/packet_client/component.adb
@@ -22,18 +22,19 @@ is
    procedure Update (Session : in out Packet.Client_Session;
                      Idx     :        Desc_Index;
                      Buf     :    out String;
-                     Ctx     : in out Descriptors);
+                     Ctx     : in out Gneiss_Log.Client_Session);
 
    procedure Read (Session : in out Packet.Client_Session;
                    Idx     :        Desc_Index;
                    Buf     :        String;
-                   Ctx     : in out Descriptors);
+                   Ctx     : in out Gneiss_Log.Client_Session);
 
-   package Packet_Client is new Packet.Client (Descriptors, Event, Update, Read);
+   package Packet_Client is new Packet.Client (Gneiss_Log.Client_Session, Event, Update, Read);
 
    Client     : Packet.Client_Session;
    Log        : Gneiss_Log.Client_Session;
    Capability : Gneiss.Capability;
+   Descs      : Descriptors;
 
    procedure Construct (Cap : Gneiss.Capability)
    is
@@ -42,7 +43,13 @@ is
       Log_Client.Initialize (Log, Capability, "log_packet");
       Packet_Client.Initialize (Client, Capability, "log");
       if Gneiss_Log.Initialized (Log) and Packet.Initialized (Client) then
-         null;
+         Packet_Client.Allocate (Client, Descs (1), 12, 1);
+         if Packet_Client.Allocated (Client, Descs (1)) then
+            Packet_Client.Update (Client, Descs (1), Log);
+            Packet_Client.Send (Client, Descs (1));
+         else
+            Main.Vacate (Capability, Main.Failure);
+         end if;
       else
          Main.Vacate (Capability, Main.Failure);
       end if;
@@ -51,25 +58,40 @@ is
    procedure Event
    is
    begin
-      null;
+      for I in Descs'Range loop
+         if not Packet_Client.Allocated (Client, Descs (I)) then
+            Packet_Client.Receive (Client, Descs (I), I);
+            exit when not Packet_Client.Allocated (Client, Descs (I));
+            Packet_Client.Read (Client, Descs (I), Log);
+            Packet_Client.Free (Client, Descs (I));
+         end if;
+      end loop;
    end Event;
 
    procedure Update (Session : in out Packet.Client_Session;
                      Idx     :        Desc_Index;
                      Buf     :    out String;
-                     Ctx     : in out Descriptors)
+                     Ctx     : in out Gneiss_Log.Client_Session)
    is
+      pragma Unreferenced (Session);
+      pragma Unreferenced (Idx);
+      pragma Unreferenced (Ctx);
    begin
-      null;
+      Buf := (others => Character'Last);
+      if Buf'Length >= 12 then
+         Buf (Buf'First .. Buf'First + 11) := "Hello World!";
+      end if;
    end Update;
 
    procedure Read (Session : in out Packet.Client_Session;
                    Idx     :        Desc_Index;
                    Buf     :        String;
-                   Ctx     : in out Descriptors)
+                   Ctx     : in out Gneiss_Log.Client_Session)
    is
+      pragma Unreferenced (Session);
+      pragma Unreferenced (Idx);
    begin
-      null;
+      Log_Client.Info (Ctx, "Packet: " & Buf);
    end Read;
 
    procedure Destruct
