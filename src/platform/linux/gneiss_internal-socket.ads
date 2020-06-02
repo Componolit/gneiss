31c5946cca1a1a337e138e1a905d0f28987a4c50
Johannes Kliemann <kliemann@componolit.com>
2020-06-02 15:22:18 +0200

    Update RecordFlux to 0.4.0
    ref #180

--- src/platform/linux/gneiss_internal-socket.ads
+++ src/platform/linux/gneiss_internal-socket.ads
@@ -1,12 +1,21 @@
 
 with Gneiss_Protocol.Session;
-with Gneiss_Protocol.Types;
+with Gneiss_Protocol.RFLX_Generic_Types;
 with Gneiss_Internal.Linux;
 
 private package Gneiss_Internal.Socket with
    SPARK_Mode,
    Abstract_State => (Packet_State with Part_Of => Gneiss_Internal.Platform_State)
 is
+
+   type Long_Natural is range 0 .. Natural'Last * 8;
+   type String_Ptr is access String;
+   package Types is new Gneiss_Protocol.RFLX_Generic_Types (Positive,
+                                                            Character,
+                                                            String,
+                                                            String_Ptr,
+                                                            Natural,
+                                                            Long_Natural);
 
    procedure Send (Fd     : File_Descriptor;
                    Action : Gneiss_Protocol.Session.Action_Type;
@@ -29,24 +38,24 @@ private
    generic
       Field : in out String;
       Last  : in out Natural;
-   procedure Get (Data : Gneiss_Protocol.Types.Bytes) with
+   procedure Get (Data : String) with
       Global => null;
 
    generic
       Field : String;
-   procedure Set (Data : out Gneiss_Protocol.Types.Bytes) with
+   procedure Set (Data : out String) with
       Global => null;
 
    procedure Send (Fd     : File_Descriptor;
-                   Buf    : Gneiss_Protocol.Types.Bytes;
+                   Buf    : String;
                    Length : Integer;
                    Fds    : Fd_Array) with
       Pre    => Valid (Fd),
       Global => (In_Out => Linux.Linux_State);
 
    procedure Recv (Fd     :     File_Descriptor;
-                   Buf    : out Gneiss_Protocol.Types.Bytes;
-                   Length : out Gneiss_Protocol.Types.Length;
+                   Buf    : out String;
+                   Length : out Natural;
                    Fds    : out Fd_Array;
                    Block  :     Boolean) with
       Pre    => Valid (Fd),
