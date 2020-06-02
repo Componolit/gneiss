5e99fa8e7e0ffe581cd6374f1780bce2d7703c2b
Johannes Kliemann <kliemann@componolit.com>
2020-06-02 15:22:18 +0200

    Update RecordFlux to 0.4.0
    ref #180

--- src/platform/linux/gneiss_internal-socket.adb
+++ src/platform/linux/gneiss_internal-socket.adb
@@ -1,17 +1,19 @@
 
-with Gneiss_Protocol.Session.PDU;
+with Gneiss_Protocol.Session.Generic_PDU;
 
 package body Gneiss_Internal.Socket with
    SPARK_Mode,
    Refined_State => (Packet_State => null)
 is
 
+   package PDU is new Gneiss_Protocol.Session.Generic_PDU (Types);
+
    generic
    package Generic_Buffer with
       SPARK_Mode
    is
 
-      Ptr : Gneiss_Protocol.Types.Bytes_Ptr;
+      Ptr : String_Ptr;
 
    end Generic_Buffer;
 
@@ -19,37 +21,41 @@ is
       SPARK_Mode
    is
 
-      Buf : aliased Gneiss_Protocol.Types.Bytes (1 .. 1024);
+      Buf : aliased String (1 .. 1024);
 
    begin
       pragma SPARK_Mode (Off);
       Ptr := Buf'Unrestricted_Access;
    end Generic_Buffer;
 
-   procedure Get (Data : Gneiss_Protocol.Types.Bytes)
+   procedure Get (Data : String)
    is
-      use type Gneiss_Protocol.Types.Length;
-      I : Natural := Field'First;
    begin
-      for J in Data'Range loop
-         Field (I) := Character'Val (Gneiss_Protocol.Types.Byte'Pos (Data (J)));
-         exit when I = Field'Last or else J = Data'Last;
-         I := I + 1;
-      end loop;
-      Last := I;
+      Last := 0;
+      if Data'Length < 1 or else Field'Length < 1 then
+         return;
+      end if;
+      if Field'Length >= Data'Length then
+         Last := Field'First + Data'Length - 1;
+         Field (Field'First .. Last) := Data;
+      else
+         Field := Data (Data'First .. Data'First + Field'Length - 1);
+         Last  := Field'Last;
+      end if;
    end Get;
 
-   procedure Set (Data : out Gneiss_Protocol.Types.Bytes)
+   procedure Set (Data : out String)
    is
-      use type Gneiss_Protocol.Types.Length;
-      I : Natural := Field'First;
    begin
-      Data := (others => Gneiss_Protocol.Types.Byte'First);
-      for J in Data'Range loop
-         Data (J) := Gneiss_Protocol.Types.Byte'Val (Character'Pos (Field (I)));
-         exit when I = Field'Last or else J = Data'Last;
-         I := I + 1;
-      end loop;
+      if Data'Length < 1 or else Field'Length < 1 then
+         return;
+      end if;
+      if Field'Length >= Data'Length then
+         Data := Field (Field'First .. Field'First + Data'Length - 1);
+      else
+         Data := (others => Character'First);
+         Data (Data'First .. Data'First + Field'Length - 1) := Field;
+      end if;
    end Set;
 
    procedure Send (Fd     : File_Descriptor;
@@ -59,26 +65,25 @@ is
                    Label  : Session_Label;
                    Fds    : Fd_Array)
    is
-      use type Gneiss_Protocol.Types.Length;
       package Buffer is new Generic_Buffer;
       procedure Buffer_Name is new Set (Name.Value (Name.Value'First .. Name.Last));
       procedure Buffer_Label is new Set (Label.Value (Label.Value'First .. Label.Last));
-      procedure Set_Name is new Gneiss_Protocol.Session.PDU.Set_Name (Buffer_Name);
-      procedure Set_Label is new Gneiss_Protocol.Session.PDU.Set_Label (Buffer_Label);
-      Context : Gneiss_Protocol.Session.PDU.Context := Gneiss_Protocol.Session.PDU.Create;
+      procedure Set_Name is new PDU.Set_Name (Buffer_Name);
+      procedure Set_Label is new PDU.Set_Label (Buffer_Label);
+      Context : PDU.Context;
    begin
-      Gneiss_Protocol.Session.PDU.Initialize (Context, Buffer.Ptr);
-      Gneiss_Protocol.Session.PDU.Set_Action (Context, Action);
-      Gneiss_Protocol.Session.PDU.Set_Kind (Context, Kind);
-      Gneiss_Protocol.Session.PDU.Set_Name_Length (Context, Gneiss_Protocol.Session.Length_Type (Name.Last));
+      PDU.Initialize (Context, Buffer.Ptr);
+      PDU.Set_Action (Context, Action);
+      PDU.Set_Kind (Context, Kind);
+      PDU.Set_Name_Length (Context, Gneiss_Protocol.Session.Length_Type (Name.Last));
       if Name.Last > 0 then
          Set_Name (Context);
       end if;
-      Gneiss_Protocol.Session.PDU.Set_Label_Length (Context, Gneiss_Protocol.Session.Length_Type (Label.Last));
+      PDU.Set_Label_Length (Context, Gneiss_Protocol.Session.Length_Type (Label.Last));
       if Label.Last > 0 then
          Set_Label (Context);
       end if;
-      Gneiss_Protocol.Session.PDU.Take_Buffer (Context, Buffer.Ptr);
+      PDU.Take_Buffer (Context, Buffer.Ptr);
       Send (Fd, Buffer.Ptr.all,
             4 + Name.Last + Label.Last,
             Fds);
@@ -89,41 +94,40 @@ is
                       Fds   : out Fd_Array;
                       Block :     Boolean)
    is
-      use type Gneiss_Protocol.Types.Length;
       use type Gneiss_Protocol.Session.Length_Type;
       package Buffer is new Generic_Buffer;
-      Context : Gneiss_Protocol.Session.PDU.Context := Gneiss_Protocol.Session.PDU.Create;
+      Context : PDU.Context;
       Action  : Gneiss_Protocol.Session.Action_Type;
       Kind    : Gneiss_Protocol.Session.Kind_Type;
       Name    : Session_Label;
       Label   : Session_Label;
-      Length  : Gneiss_Protocol.Types.Length;
+      Length  : Natural;
       procedure Parse_Name is new Get (Name.Value, Name.Last);
       procedure Parse_Label is new Get (Label.Value, Label.Last);
-      procedure Get_Name is new Gneiss_Protocol.Session.PDU.Get_Name (Parse_Name);
-      procedure Get_Label is new Gneiss_Protocol.Session.PDU.Get_Label (Parse_Label);
+      procedure Get_Name is new PDU.Get_Name (Parse_Name);
+      procedure Get_Label is new PDU.Get_Label (Parse_Label);
    begin
       Msg := Broker_Message'(Valid => False);
       Recv (Fd, Buffer.Ptr.all, Length, Fds, Block);
       if Length = 0 then --  EOF
          return;
       end if;
-      Gneiss_Protocol.Session.PDU.Initialize (Context, Buffer.Ptr);
-      Gneiss_Protocol.Session.PDU.Verify_Message (Context);
-      if not Gneiss_Protocol.Session.PDU.Structural_Valid_Message (Context) then
+      PDU.Initialize (Context, Buffer.Ptr);
+      PDU.Verify_Message (Context);
+      if not PDU.Structural_Valid_Message (Context) then
          return;
       end if;
-      Action := Gneiss_Protocol.Session.PDU.Get_Action (Context);
-      Kind := Gneiss_Protocol.Session.PDU.Get_Kind (Context);
+      Action := PDU.Get_Action (Context);
+      Kind   := PDU.Get_Kind (Context);
       if
-         Gneiss_Protocol.Session.PDU.Get_Name_Length (Context) > 0
-         and then Gneiss_Protocol.Session.PDU.Present (Context, Gneiss_Protocol.Session.PDU.F_Name)
+         PDU.Get_Name_Length (Context) > 0
+         and then PDU.Present (Context, PDU.F_Name)
       then
          Get_Name (Context);
       end if;
       if
-         Gneiss_Protocol.Session.PDU.Get_Label_Length (Context) > 0
-         and then Gneiss_Protocol.Session.PDU.Present (Context, Gneiss_Protocol.Session.PDU.F_Label)
+         PDU.Get_Label_Length (Context) > 0
+         and then PDU.Present (Context, PDU.F_Label)
       then
          Get_Label (Context);
       end if;
@@ -132,11 +136,11 @@ is
                              Kind   => Kind,
                              Name   => Name,
                              Label  => Label);
-      Gneiss_Protocol.Session.PDU.Take_Buffer (Context, Buffer.Ptr);
+      PDU.Take_Buffer (Context, Buffer.Ptr);
    end Receive;
 
    procedure Send (Fd     : File_Descriptor;
-                   Buf    : Gneiss_Protocol.Types.Bytes;
+                   Buf    : String;
                    Length : Integer;
                    Fds    : Fd_Array) with
       SPARK_Mode => Off
@@ -146,17 +150,18 @@ is
    end Send;
 
    procedure Recv (Fd     :     File_Descriptor;
-                   Buf    : out Gneiss_Protocol.Types.Bytes;
-                   Length : out Gneiss_Protocol.Types.Length;
+                   Buf    : out String;
+                   Length : out Natural;
                    Fds    : out Fd_Array;
-                   Block  :     Boolean)
+                   Block  :     Boolean) with
+      SPARK_Mode => Off
    is
       Ignore_Trunc : Integer;
       Len          : Integer;
    begin
       Linux.Read_Message (Fd, Buf'Address, Buf'Length, Fds, Fds'Length,
                           Len, Ignore_Trunc, Boolean'Pos (Block));
-      Length := (if Len > 0 then Gneiss_Protocol.Types.Length (Len) else 0);
+      Length := (if Len > 0 then Natural (Len) else 0);
    end Recv;
 
 end Gneiss_Internal.Socket;
