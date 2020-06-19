0a94ce471a3aa43d78c3683b91c0311ebf737fb6
Johannes Kliemann <kliemann@componolit.com>
2020-06-19 15:16:53 +0200

    Add stream session startup
    ref #184

--- src/stream/linux/gneiss_internal-stream.ads
+++ src/stream/linux/gneiss_internal-stream.ads
@@ -1,22 +1,37 @@
+with Gneiss;
 
 package Gneiss_Internal.Stream with
    SPARK_Mode
 is
 
    type Client_Session is limited record
-      null;
+      Fd    : File_Descriptor             := -1;
+      Efd   : Epoll_Fd                    := -1;
+      Index : Gneiss.Session_Index_Option := Gneiss.Session_Index_Option'(Valid => False);
+      E_Cap : Event_Cap                   := Invalid_Event_Cap;
    end record;
 
    type Server_Session is limited record
-      null;
+      Fd    : File_Descriptor             := -1;
+      Index : Gneiss.Session_Index_Option := Gneiss.Session_Index_Option'(Valid => False);
+      E_Cap : Event_Cap                   := Invalid_Event_Cap;
    end record;
 
    type Dispatcher_Session is limited record
-      null;
+      Broker_Fd   : File_Descriptor             := -1;
+      Dispatch_Fd : File_Descriptor             := -1;
+      Efd         : Epoll_Fd                    := -1;
+      Index       : Gneiss.Session_Index_Option := Gneiss.Session_Index_Option'(Valid => False);
+      E_Cap       : Event_Cap                   := Invalid_Event_Cap;
+      Accepted    : Boolean                     := False;
    end record;
 
    type Dispatcher_Capability is limited record
-      null;
+      Client_Fd : File_Descriptor := -1;
+      Server_Fd : File_Descriptor := -1;
+      Clean_Fd  : File_Descriptor := -1;
+      Name      : Session_Label;
+      Label     : Session_Label;
    end record;
 
 end Gneiss_Internal.Stream;
